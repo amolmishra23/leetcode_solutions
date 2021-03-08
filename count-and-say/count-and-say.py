275a8081bca96589e6c6c94133c3bbfaaf44861e
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-03-08 07:04:48 +0530

    Time: 52 ms (37.29%), Space: 14.6 MB (6.55%) - LeetHub

---
+++ count-and-say/count-and-say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def solve(seq):
+            i, new_seq = 0, []
+            while i<len(seq):
+                count = 1
+                while i<len(seq)-1 and seq[i]==seq[i+1]:
+                    count += 1
+                    i += 1
+                new_seq.extend([str(count), seq[i]])
+                i+=1
+            return new_seq
+            
+        seq=["1"]
+        for i in range(n-1):
+            seq = solve(seq)
+        return "".join(seq)
