305a2a7b8d576552ddbdd69911c6a7f425a96a9b
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-03-25 10:14:02 +0530

    Time: 68 ms (99.69%), Space: 15.5 MB (93.09%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,19 +1,5 @@
+import itertools
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def solve(arr, low, high, curr, res):
-            if len(curr)==k:
-                res.append(list(curr))
-                return
-            
-            if low>high:
-                return
-            
-            curr.append(arr[low])
-            solve(arr, low+1, high, curr, res)
-            curr.pop()
-            
-            solve(arr, low+1, high, curr, res)
-            
-        res = []
-        solve(list(range(1, n+1)), 0, n-1, [], res)
-        return res
+        return list(itertools.combinations(range(1,n+1), k))
