26a44b170156bbabd3e6c5040f9b41999d530225
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-04-06 19:16:10 +0530

    Time: 280 ms (5.18%), Space: 86.4 MB (5.18%) - LeetHub

---
+++ distinct-subsequences/distinct-subsequences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        @functools.lru_cache(None)
+        def solve(i, j):
+            if j<0: return 1
+            if i<0: return 0
+            return solve(i-1,j)+solve(i-1,j-1) if s[i]==t[j] else solve(i-1,j)
+        
+        return solve(len(s)-1, len(t)-1)
