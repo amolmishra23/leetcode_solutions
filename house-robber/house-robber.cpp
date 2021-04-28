53e49f7f90242ff375c14dc866ca944082b297d4
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-04-28 07:25:46 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (35.71%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -5,6 +5,9 @@ public:
             return 0;
         if (dp[idx]>=0)
             return dp[idx];
+        // either we can steal from 2 homes before+this house.
+        // or skip this house and steal from 1 house before. 
+        // that gives us the most optimized answer till that house.n 
         dp[idx] = max(solve(nums, idx-2, dp)+nums[idx], solve(nums, idx-1, dp));
         return dp[idx];
     }
