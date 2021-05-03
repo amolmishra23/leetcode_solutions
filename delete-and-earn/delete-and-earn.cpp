600c602a2a40ace62b6be20f147740ad6ad2ef85
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-05-03 08:54:58 +0530

    Time: 4 ms (93.43%), Space: 9.2 MB (73.23%) - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -7,7 +7,6 @@ public:
             return nums[0];
         
         int max_val = *max_element(nums.begin(), nums.end());
-        
         vector<int> counter(max_val+1, 0), dp(max_val+1, 0);
         
         for (int &i: nums)
@@ -15,18 +14,19 @@ public:
         
         int res = 0;
         
-        if (nums[1]!=0){
+        if (nums[1]!=0) {
             dp[1] = counter[1];
             res = counter[1];
         }
         
         for (int i=2; i<max_val+1; i++) {
-            if (counter[i] == 0)
+            if (counter[i]==0)
                 dp[i] = dp[i-1];
             else
                 dp[i] = max(dp[i-1], dp[i-2]+i*counter[i]);
             res = max(res, dp[i]);
         }
+        
         return res;
     }
 };
