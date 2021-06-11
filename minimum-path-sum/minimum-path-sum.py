5d191b4344cd1fcd3cd87cc6d1755128dd04de8a
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-11 21:44:47 +0530

    Time: 96 ms (77.19%), Space: 15.7 MB (61.39%) - LeetHub

--- minimum-path-sum/minimum-path-sum.py
+++ minimum-path-sum/minimum-path-sum.py
@@ -1,11 +1,18 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        """
+        Aim in this question being, to find shortest path.
+        We need to break it down and see which is shorter. Whether the left one or top one.
+        And we can conclude on our answer. 
+        """
         m, n = len(grid), len(grid[0])
-        dp = [[0]*n for _ in range(m)]
+        
+        dp = [[None]*n for _ in range(m)]
         
         for i in range(m):
             for j in range(n):
                 if i==0 and j==0: dp[i][j]=grid[i][j]
-                else: dp[i][j] = min(dp[i-1][j] if i>0 else float('inf'), dp[i][j-1] if j>0 else float('inf')) + grid[i][j]
+                else: 
+                    dp[i][j] = min(dp[i-1][j] if i>=1 else float('inf'), dp[i][j-1] if j>=1 else float('inf')) + grid[i][j]
         
-        return dp[m-1][n-1]
+        return dp[-1][-1]
