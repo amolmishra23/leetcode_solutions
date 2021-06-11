90db2993e6746caeccf2a0d93d22c1e521d24d49
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-11 21:51:47 +0530

    Time: 60 ms (66.51%), Space: 15.2 MB (24.48%) - LeetHub

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,18 +1,16 @@
 class Solution:
     def minimumTotal(self, arr: List[List[int]]) -> int:
-        if len(arr)==0: return 0
-        rows = len(arr)
-        """
-        First we got to cache, min path to reach last row
-        From r-1 to r, we will move based on whichever is minimum at c, c+1
-        We do this logic via DP
-        dp[j] = min(dp[j], dp[j+1]) + arr[i][j]
-        """
+        if not arr: return 0
+        
+        m, n = len(arr), len(arr[0])
+        # we basically come from bottom to up
+        # at every level, just check whichever path is cheaper to go. 
+        # its cheaper to go from 6 to 1, rather than 6 to 4. 
+        # finally we have the answer, which route to traverse from the top. 
         dp = list(arr[-1])
         
-        for i in range(rows-2, -1, -1):
+        for i in range(m-2, -1, -1):
             for j in range(i+1):
                 dp[j] = min(dp[j], dp[j+1]) + arr[i][j]
                 
         return dp[0]
-        
