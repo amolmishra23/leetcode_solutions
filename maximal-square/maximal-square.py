f55f90385d276d62662fc61d4dcb4e741c685aa8
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-14 22:05:45 +0530

    Time: 212 ms (54.35%), Space: 15.7 MB (49.60%) - LeetHub

--- maximal-square/maximal-square.py
+++ maximal-square/maximal-square.py
@@ -1,16 +1,18 @@
 class Solution:
-    def maximalSquare(self, matrix: List[List[str]]) -> int:
+    def maximalSquare(self, arr: List[List[str]]) -> int:
         """
-        In order to calculate the area, verify if we have a square from left, diagonal and top. 
-        If yes, whatever is the most minimal value of square, we will calculate our area based on that + 1, at current cell and the level. 
+        How can we close a square? 
+        Basically all the 3 vertices also should have 1. 
+        If all are itself a square, then even easier to close square. 
         """
-        if len(matrix)==0: return 0
-        dp = [[0]*(len(matrix[0])+1) for _ in range(len(matrix)+1)]
-        res_ = 0
+        if len(arr)==0: return 0
+        m, n = len(arr), len(arr[0])
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        res_=0
         
-        for i in range(1, len(matrix)+1):
-            for j in range(1, len(matrix[0])+1):
-                if matrix[i-1][j-1]=="1":
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if arr[i-1][j-1]=="1":
                     dp[i][j] = 1+min(
                         dp[i-1][j-1] if i>=1 and j>=1 else 0,
                         dp[i-1][j] if i>=1 else 0,
@@ -19,3 +21,4 @@ class Solution:
                     res_ = max(res_, dp[i][j])
         
         return res_*res_
+        
