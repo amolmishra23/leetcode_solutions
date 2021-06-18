0ab869775fc0ec2cce7e764393fb735092ef597f
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-18 22:26:04 +0530

    Time: 40 ms (51.65%), Space: 14.6 MB (62.69%) - LeetHub

--- h-index/h-index.py
+++ h-index/h-index.py
@@ -10,6 +10,10 @@ class Solution:
         Example being [0,1,3,5,6]
         if arr[2]=3, means after that we have 3 more numbers left(n-mid). 
         3 is the h-index. 
+        
+        Balancing point is determined by if arr[mid] is equal to number of elements left after it. 
+        If arr[mid] is bigger is no of elements, considering its sorted, our answer surely lies in the left side. 
+        If arr[mid] is smaller, our answer surely lies in the right side. 
         """
         while low<=high:
             mid = low + (high-low)//2
