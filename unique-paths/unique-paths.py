afbbaa9e356f144c01aeae3eddc4709173072a44
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-24 09:02:22 +0530

    Time: 24 ms (96.32%), Space: 14.3 MB (36.44%) - LeetHub

--- unique-paths/unique-paths.py
+++ unique-paths/unique-paths.py
@@ -1,5 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        """
+        Basic idea is that we can reach at (i,j) either from (i-1,j) and (i,j-1)
+        So we basically add up how many ways we can reach both of them. And that becomes our solution to the problem. 
+        """
         dp = [[0]*n for _ in range(m)]
         
         for i in range(m):
