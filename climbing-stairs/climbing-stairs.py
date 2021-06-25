3cd5a77114002755c6e06cc15dc18cd637a4fdad
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-06-25 18:27:35 +0530

    Time: 32 ms (54.08%), Space: 14.3 MB (10.61%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -3,7 +3,8 @@ class Solution:
         """
         If 1 stair, we can only climb 1 way
         if 2 stairs, we can climb either 1+1 or we can climb 2. 
-        For 3 stairs, we can either come from n-2(1) or n-1(2), whatever ways was possible until that. 
+        For 3 stairs, we can either come from n-2(1) and n-1(2), whatever ways was possible until that. 
+        Thats the speciality of only 3. We can reach from 1, 2. Hence add up the possible ways. 
         Like this we form the recursion tree and solve the question. 
         """
         if n<=2: return n
