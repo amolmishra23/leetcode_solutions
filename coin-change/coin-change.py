086d19d42654d4df75fda1dd2bd326f67af18aea
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-07-03 13:06:20 +0530

    Time: 3116 ms (5.02%), Space: 484.1 MB (5.26%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -1,24 +1,16 @@
 class Solution:
-    def coinChange(self, arr: List[int], n: int) -> int:
-        m = len(arr)
-        
-        dp = [[None]*(n+1) for _ in range(m+1)]
-        """
-        dp table rows are basically using until xth coins
-        dp table cols are basically for that particular amount y
-        Lets think of a few questions. 
-        Min coins to make 0,1,2,...n amount. when 0 coins avlb. float('inf')
-        Min coins to make amount 0 when we have supply of 1,2..n coins. 0
-        Min coins to make amount 0, when we have 0 coins. 0
-        """
-            
-        for i in range(m+1):
-            for j in range(n+1):
-                if i==0: dp[i][j] = float('inf')
-                elif j==0: dp[i][j] = 0
-                elif (arr[i-1] <= j):
-                    dp[i][j] = min(dp[i-1][j], 1+dp[i][j-arr[i-1]])
-                else:
-                    dp[i][j] = dp[i-1][j]
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def coin_change(coins, idx, target):
+          if (idx, target) in cache: return cache[idx, target]
+          if target==0: return 0;
 
-        return dp[m][n] if dp[m][n]!=float('inf') else -1
+          if target<0 or idx>=len(coins): return float('inf')
+
+          cache[idx, target] = min(1+coin_change(coins, idx, target-coins[idx]), coin_change(coins, idx+1, target))
+
+          return cache[idx,target]
+    
+        cache={}
+        res = coin_change(coins, 0, amount)
+        
+        return res if res!=float('inf') else -1
