37db44a267678f2ae5e369ded8a1983f90fc51bc
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-07-11 23:14:53 +0530

    Time: 52 ms (57.16%), Space: 14.4 MB (30.29%) - LeetHub

--- 2-keys-keyboard/2-keys-keyboard.py
+++ 2-keys-keyboard/2-keys-keyboard.py
@@ -1,26 +1,25 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        """
-        In case of 1, we already have it on screen, hence answer is 0
-        In case of 2, we copy once, and paste once. Hence answer is 2. 
-        """
-        if n==1: return 0
-        if n==2: return 2
+        @functools.lru_cache(None)
+        def solve(n):
+            """
+            In case of 1, we already have it on screen, hence answer is 0
+            In case of 2, we copy once, and paste once. Hence answer is 2. 
+            """
+            if n==1: return 0
+            if n==2: return 2
+            
+            """
+            Even for 3, its 3 operations, simple. 
+            But for higher numbers, to perform copy paste, it needs to be a multiple, to paste exactly that many times.
+            Hence we find all the multiples of that number, and check is it worth using that multiple, and pasting the multiple required times. Like for 9, we can get to 3. And copy 3, and pasten 3,3. Hence we have 9. 
+            
+            To get 9, we can also put 3 in 3 steps.
+            Then we copy the 3, and paste it twice. Hence 6 steps
+            """
+            for i in range(2, int(math.sqrt(n))+1):
+                if n%i==0: return i+solve(n//i)
+                
+            return n
         
-        prev = [None]*(n+1)
-        prev[1], prev[2] = 0, 2
-        
-        """
-        Even for 3, its 3 operations, simple. 
-        But for higher numbers, to perform copy paste, it needs to be a multiple, to paste exactly that many times.
-        Hence we find all the multiples of that number, and check is it worth using that multiple, and pasting the multiple required times. Like for 9, we can get to 3. And copy 3, and pasten 3,3. Hence we have 9. 
-        """
-        for i in range(3, n+1):
-            prev[i] = i
-            # finding all the multiples of i. 
-            # for 12, the multiples are 6, 4, 3, 2. Hence we try to find by pasting of all the possible multiples. 
-            for j in range(i//2, 0, -1):
-                # if a number perfectly divides it
-                if (i%j==0): prev[i] = min(prev[i], prev[j]+i//j)
-                    
-        return prev[n]
+        return solve(n)
