62bdf16f2170a6ad668c01c4c85f64c9d9a5cd07
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-07-13 22:13:18 +0530

    Time: 356 ms (22.89%), Space: 58.3 MB (5.97%) - LeetHub

---
+++ uncrossed-lines/uncrossed-lines.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        @functools.lru_cache(None)
+        def lcs(i, j):
+            if i>=len(nums1) or j>=len(nums2): return 0
+            
+            if nums1[i]==nums2[j]: return 1+lcs(i+1, j+1)
+            
+            return max(lcs(i, j+1), lcs(i+1,j))
+        
+        return lcs(0,0)
