215a9392c4a334875ab3dafbf4b2769010b6209c
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-08-22 12:13:51 +0530

    Time: 28 ms (83.92%), Space: 14.3 MB (9.18%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,9 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.cache = {0:0, 1:1}
-        
     def fib(self, n: int) -> int:
-        if n in self.cache: return self.cache[n]
-        if n-1 not in self.cache: self.cache[n-1] = self.fib(n-1)
-        if n-2 not in self.cache: self.cache[n-2] = self.fib(n-2)
-        return self.cache[n-1]+self.cache[n-2]
+        cache = [None]*(31)
+        cache[0] = 0
+        cache[1] = 1
+        
+        def solve(n):
+            if cache[n] is not None: return cache[n]
+            
+            cache[n] = solve(n-1)+solve(n-2)
+
+            return cache[n]
+        
+        return solve(n)
