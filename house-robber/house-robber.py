b947a92346b78f809e97b90a5a1ff4ed3164e947
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-05 08:39:52 +0530

    Time: 40 ms (21.44%), Space: 14.2 MB (77.57%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -2,14 +2,17 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums: return 0
         
+        # robbing house at ith index gives us 0
+        # question is, which other alternate houses to rob, and which particular permutation sequence gives us max profit?
+        # we use dp to solve the same
+        
         if len(nums)<=2: return max(nums)
         
         dp = [0]*len(nums)
         dp[0] = nums[0]
         dp[1] = max(nums[:2])
         
-        for x in range(2, len(nums)):
-            dp[x] = max(dp[x-1], nums[x]+dp[x-2])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2])
             
         return dp[-1]
-        
