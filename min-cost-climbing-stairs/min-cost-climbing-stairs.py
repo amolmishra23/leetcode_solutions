01a789a20e7934f9b1db785ed26dd4735a6c0dea
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-05 08:36:02 +0530

    Time: 76 ms (20.42%), Space: 14.5 MB (22.09%) - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,16 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # question is given wrong, aim is not to reach last block. but next to last block. 
         dp = [float('inf')]*len(cost)
         
-        # first one obvious hai
-        dp[0] = cost[0]
-        # to reach dp[2], either dp[0]+dp[1] or directly dp[1]. Hence this is also obvious. 
-        dp[1] = cost[1]
+        # to reach ith place, we will obv have to spend cost[i]
+        # the question is, do we jump from i-1 or i-2 th places
+        # thats based on dp whichever comes minimum
+        
+        dp[0] = cost[0]+0
+        dp[1] = cost[1]+0
         
         for i in range(2, len(cost)):
-            # for everything else, we basically add current cost and see which is cheaper to come from
             dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        
-        #last one can be reached either from last or 2nd last
+            
         return min(dp[-1], dp[-2])
+        
+        
