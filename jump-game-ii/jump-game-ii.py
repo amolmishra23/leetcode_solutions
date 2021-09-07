250a1ff933b44d5feee2b2d6927b7b97a4e98124
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-07 07:31:14 +0530

    Time: 221 ms (44.37%), Space: 15.4 MB (26.62%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        next_max, res = 0, 0
+        
+        """
+        We keep running 2 parallel pointers. 
+        We try to make a jump (need to find which is the max sized jump we can make)
+        In the end we return which is the number of jumps we make
+        This we do, as long as we didnt reach the last element. 
+        """
+        
+        while fast<len(nums)-1:
+            while slow<=fast:
+                next_max = max(next_max, slow+nums[slow])
+                slow += 1
+            res += 1
+            fast = next_max
+            
+        return res
