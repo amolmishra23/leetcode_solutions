b8523e93b4e2089fcf2e107070365e141aead7e1
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-08 20:02:05 +0530

    Time: 876 ms (34.96%), Space: 27.9 MB (25.47%) - LeetHub

--- shifting-letters/shifting-letters.py
+++ shifting-letters/shifting-letters.py
@@ -1,24 +1,8 @@
 class Solution:
-    def shift_char(self, c, num):
-        char, num = ord(c), num%26
-        return chr(char+num) if not char+num>122 else chr(char-(26-num))
-        # return chr(ord(c) + (num%26))
+    def func(self, c, shift):
+        return chr((ord(c)-97+shift)%26 + 97)
     
-    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        n = len(s)
-        if n==1: return self.shift_char(s[0], shifts[0])
-        
-        postfix_sum = [0]*n
-        postfix_sum[-1] = shifts[-1]
-        
-        for i in range(n-2, -1, -1):
-            postfix_sum[i] += postfix_sum[i+1] + shifts[i]
-            
-        print(postfix_sum)
-        res = [None]*n
-        for i in range(n):
-            res[i] = self.shift_char(s[i], postfix_sum[i])
-            
-        return ''.join(res)
-        
+    def shiftingLetters(self, S: str, shifts: List[int]) -> str:
+        for i in range(len(shifts)-2, -1, -1): shifts[i] += shifts[i+1]
+        return "".join(self.func(c, s) for c,s in zip(S,shifts))
         
