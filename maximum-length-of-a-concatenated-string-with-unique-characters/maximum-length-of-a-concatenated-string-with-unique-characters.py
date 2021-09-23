9a3a85ed19db76f002064c6470dad783de0e2a93
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-23 13:24:34 +0530

    Time: 88 ms (88.82%), Space: 58 MB (9.00%) - LeetHub

--- maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,10 +1,21 @@
 class Solution:
     def maxLength(self, A):
+        # declaring an empty set already in dp
         dp = [set()]
+        
         for a in A:
+            # checking if the current element already has repeated elements
             if len(set(a)) < len(a): continue
+            
+            # create a set out of curr string
             a = set(a)
+            
+            # comparing to all prev strings in dp
             for c in dp[:]:
+                # if they have any char in common, continue
                 if a & c: continue
+                # else add all the unique chars in both the strings 
                 dp.append(a | c)
+        
+        # return the longest length string, containing unique characters. 
         return max(len(a) for a in dp)
