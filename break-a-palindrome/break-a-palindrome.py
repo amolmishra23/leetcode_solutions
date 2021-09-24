e6c18f4f6009a648dbe41b0a6dc683d8ccf78866
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-09-24 20:28:51 +0530

    Time: 32 ms (59.00%), Space: 14.3 MB (46.47%) - LeetHub

--- break-a-palindrome/break-a-palindrome.py
+++ break-a-palindrome/break-a-palindrome.py
@@ -1,6 +1,11 @@
 class Solution:
      def breakPalindrome(self, S):
+        # this is a pretty easy question
+        # find the first non 'a' to be replaced as 'a' character
         for i in range(len(S) // 2):
             if S[i] != 'a':
                 return S[:i] + 'a' + S[i + 1:]
+        
+        # if we reached this step, means entire string is full of 'a' chars only
+        # so, we basically change the last char to 'b'
         return S[:-1] + 'b' if S[:-1] else ''
