d3640f75a177d969369ea3688fcf5235e1a14f6a
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-10-03 11:11:44 +0530

    Time: 460 ms (93.66%), Space: 15.3 MB (70.54%) - LeetHub

--- jump-game/jump-game.py
+++ jump-game/jump-game.py
@@ -1,13 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        """
-        We just need to check, if we can reach nth node, from any node in the world.
-        To reach from ith node, we should have a budget greater than or equal to that node. 
-        then we can form the dp recurrence relation to reach it. 
-        """
         last_pos = len(nums)-1
-        
-        for i in range(len(nums)-2, -1, -1):
-            if i+nums[i] >= last_pos: last_pos=i
-                
+        for i in range(len(nums)-2, -1, -1): 
+            if i+nums[i]>=last_pos: last_pos = i
         return last_pos==0
