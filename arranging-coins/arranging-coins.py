1b957b2cab6992f59141a04758359127ce8ce5fb
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-11-05 22:00:36 +0530

    Time: 28 ms (97.31%), Space: 14.3 MB (7.29%) - LeetHub

--- arranging-coins/arranging-coins.py
+++ arranging-coins/arranging-coins.py
@@ -1,4 +1,21 @@
 class Solution:
+    """
+    This is an arithmetic sequence. To find the sum S of an arithmetic series , the formula is :
+    S = ((a + b)* h) /2
+    Where:
+    a is the number of coins on the first line
+    b is the number of coins of the last line
+    h is the number of lines we have (we want to find h)
+    We note that a is always 1 because we start with one coin
+    Therefore a = 1
+    Note that the number of coins on the last line is always the same as the the number of total lines.
+    Therefore: b = h
+    The equation can then be simplified into:
+    S = (1 + h)*h / 2
+    Since we want only full lines, we want the sum S to be smaller than n, the total amount of coins in the question.
+    Therefore:
+    (1 + h)*h / 2 <= n
+    """
     def arrangeCoins(self, n: int) -> int:
         left, right = 0, n
         
@@ -9,5 +26,6 @@ class Solution:
             if curr==n: return mid
             elif curr<n: left = mid+1
             else: right = mid-1
-        
+                
         return right
+            
