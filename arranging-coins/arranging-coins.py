7048a001207307d81efe84ce9aa0def9d36061c9
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-11-06 09:12:18 +0530

    Time: 32 ms (91.71%), Space: 14.3 MB (7.25%) - LeetHub

--- arranging-coins/arranging-coins.py
+++ arranging-coins/arranging-coins.py
@@ -1,31 +1,26 @@
 class Solution:
-    """
-    This is an arithmetic sequence. To find the sum S of an arithmetic series , the formula is :
-    S = ((a + b)* h) /2
-    Where:
-    a is the number of coins on the first line
-    b is the number of coins of the last line
-    h is the number of lines we have (we want to find h)
-    We note that a is always 1 because we start with one coin
-    Therefore a = 1
-    Note that the number of coins on the last line is always the same as the the number of total lines.
-    Therefore: b = h
-    The equation can then be simplified into:
-    S = (1 + h)*h / 2
-    Since we want only full lines, we want the sum S to be smaller than n, the total amount of coins in the question.
-    Therefore:
-    (1 + h)*h / 2 <= n
-    """
     def arrangeCoins(self, n: int) -> int:
+        """
+        We need to correctly find the value of k(number of full rows), while accomodating n coins.
+        Now sequentially we can do in O(n) without much trouble
+        In order to make it O(logn), we can retort to binary search
+        
+        Everytime make a guess for k. And find number of coins, in k rows.
+        If that number of coins is less than n, our answer is in right sub-array. Else in left-subarray
+        Eventually if we get equal, that is our k. (most optimal)
+        Else if we get out of loop, left>right. So answer is right(as those rows were surely complete)
+        """
         left, right = 0, n
         
         while left<=right:
-            mid = left + (right-left)//2
-            curr = mid * (mid+1)//2
+            mid = left + (right-left)//2 # this is for k
+            curr = mid * (mid+1)//2 # number of coins in k rows. Simple n*(n+1)//2 formula.
             
+            # conditions applied
             if curr==n: return mid
             elif curr<n: left = mid+1
             else: right = mid-1
-                
+        
+        # returning number of full rows. 
         return right
             
