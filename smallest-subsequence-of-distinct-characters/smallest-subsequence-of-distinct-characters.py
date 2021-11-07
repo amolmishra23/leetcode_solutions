29c7fab88750a7eed00f09290c7fb884e48f2a08
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-11-07 10:10:51 +0530

    Time: 36 ms (58.70%), Space: 14.2 MB (79.35%) - LeetHub

---
+++ smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        last_occ = {c:i for i, c in enumerate(s)}
+        stack = ["!"]
+        visited = set()
+        
+        for i, symbol in enumerate(s):
+            # one of the lower lexicographical character that we already visited
+            if symbol in visited: continue
+            
+            # remove all such elements which have occured sometime later and 
+            # are lexicographically smaller
+            while symbol<stack[-1] and last_occ[stack[-1]]>i: visited.remove(stack.pop())
+                
+            # add this element to stack 
+            stack.append(symbol)
+            
+            # add this element to the visited set
+            visited.add(symbol)
+            
+        return "".join(stack)[1:]
