bdeb19caf181f3bb5253642cb6024f7e1271531c
Amol Mishra <amolsuraj.vit5@gmail.com>
2021-12-05 08:39:12 +0530

    Time: 103 ms (5.30%), Space: 18.6 MB (7.40%) - LeetHub

---
+++ house-robber-iii/house-robber-iii.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        @functools.lru_cache(None)
+        def solve(node, parent_chosen):
+            if node is None: return 0
+            if parent_chosen:
+                # parent node was used.
+                # we cannot use the child node
+                # directly return skipping the child node. 
+                return solve(node.left, False) + solve(node.right, False)
+            else:
+                # parent node is not used
+                # we can either choose this node, or not choose this node. 
+                # we see which of those options gives us a better answer, and return accordingly. 
+                return max(
+                    node.val + solve(node.left,True) + solve(node.right, True),
+                    solve(node.left, False) + solve(node.right, False)
+                )
+            
+        return solve(root, False)
