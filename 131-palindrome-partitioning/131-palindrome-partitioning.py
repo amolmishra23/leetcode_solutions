3f156f574ec3dcc0727f3a2a37d6742140b5b7d1
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-01-05 12:36:03 +0530

    Time: 1139 ms (6.04%), Space: 26.8 MB (72.22%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palindrome(i, j): #checks if the string s[i to j] is palindrome or not
+            while i<j:
+                if s[i]!=s[j]: return False
+                i+=1
+                j-=1
+            return True
+        
+        def palindrome_part(i, parts, curr_part):
+            if i == len(s): #if parsed the entire string append the current partition list to answer
+                parts.append(curr_part)
+            
+            for j in range(i, len(s)): #try all strings starting at i and ending at j where j = (i, len(s))
+                if is_palindrome(i, j): #if the substring is palindrome then
+                    palindrome_part(j+1, parts, curr_part + [s[i:j+1]]) #add substring to temporary substring list repeat the same process from the index after the palindrome substring ends
+            
+        parts = []
+        palindrome_part(0, parts, [])
+        return parts
