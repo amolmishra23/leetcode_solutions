bee13dfdf8c1d35666df87c8c36226a2e76e3c8e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-01-21 09:15:36 +0530

    Time: 448 ms (94.91%), Space: 19 MB (51.22%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,5 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        """
+        No need to worry about cities in between, and recount from there. Even they will fail mostly.
+        As from i,i+1...j. If at j we were out of fuel, definitely will be out of fuel at i+1 also. 
+        
+        Because we are guaranteed to have an answer, hence start everytime from very next index.
+        Wherever we stopped, because of fuel insufficiency. 
+        """
+        
         if len(gas)==0 or len(cost)==0 or sum(gas)<sum(cost): return -1
         
         pos, bal = 0, 0
