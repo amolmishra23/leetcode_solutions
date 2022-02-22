ce60976d4c222cebcbbc34519626b1b8abcecc21
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-22 22:13:40 +0530

    Time: 100 ms (61.98%), Space: 14.1 MB (57.75%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,18 +1,17 @@
 class Solution:
-    # this problem is to be solved using unbounded knapsack
-    def combinationSum(self, can: List[int], target: int) -> List[List[int]]:
-        def solve(i, target, currpath):
+    def combinationSum(self, arr: List[int], target: int) -> List[List[int]]:
+        def solve(idx, curr, target):
             if target==0:
-                self.res.add(tuple(currpath))
-                return 
+                self.res.append(list(curr))
+                return
             
-            for i in range(i, len(can)):
-                if can[i]>target: break
-                currpath.append(can[i])
-                solve(i, target-can[i], currpath)
-                currpath.pop()
-                
-        self.res = set()
-        can.sort()
-        solve(0, target, [])
+            if target<0 or idx>=len(arr): return 
+            
+            solve(idx+1, curr, target)
+            curr.append(arr[idx])
+            solve(idx, curr, target-arr[idx])
+            curr.pop()
+        
+        self.res = []
+        solve(0, [], target)
         return self.res
