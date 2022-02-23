d249871196b2669b025cef11d18a9443e423a0d9
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-23 19:36:41 +0530

    Time: 563 ms (37.30%), Space: 15 MB (94.64%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,20 +1,29 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        word_set = set(wordList)
-        if endWord not in word_set: return 0
+        wordSet = set(wordList)
+        if beginWord in wordSet:
+          wordSet.remove(beginWord)
         
-        q = deque([(beginWord, 1)])
-        chars = {x for word in wordList for x in word}       
+        def neighbors(s):
+            for i in range(len(s)):
+                chars = list(s)
+                for c in ascii_lowercase:
+                    chars[i] = c
+                    newStr = "".join(chars)
+                    if newStr in wordSet:
+                        yield newStr
         
+        ans = 0
+        q = deque([beginWord])
         while q:
-            word, level = q.popleft()
-            if word == endWord: return level
-            
-            for i in range(len(word)):
-                for c in chars:
-                    temp = word[:i]+c+word[i+1:]
-                    if temp in word_set:
-                        q.append((temp, level+1))
-                        word_set.remove(temp)
-                        
-        return 0
+            ans += 1
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur == endWord:
+                    return ans
+
+                for newStr in neighbors(cur):
+                    q.append(newStr)
+                    wordSet.remove(newStr)
+                    
+        return 0  # no such transformation sequence.
