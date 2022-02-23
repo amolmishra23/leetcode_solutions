99052d31e60b7ab9bdc0b2c102ab9ce690a3c987
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-23 19:52:03 +0530

    Time: 1907 ms (38.35%), Space: 483.9 MB (5.02%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,21 +1,20 @@
 class Solution:
     def minCut(self, s: str) -> int:
-        n = len(s)
+        @lru_cache(None)
+        def is_pal(l,r):
+          if l>=r: return True
+          if s[l]!=s[r]: return False
+          return is_pal(l+1,r-1)
         
         @lru_cache(None)
-        def isPalindrome(l, r):  # l, r inclusive
-            if l >= r: return True
-            if s[l] != s[r]: return False
-            return isPalindrome(l+1, r-1)
+        def solve(i):
+          if i==len(s): return 0
+          
+          ans = float('inf')
+          for j in range(i, len(s)):
+            if is_pal(i, j):
+              ans = min(ans, solve(j+1)+1)
+              
+          return ans
         
-        @lru_cache(None)
-        def dp(i):  # s[i..n-1]
-            if i == n:
-                return 0
-            ans = math.inf
-            for j in range(i, n):
-                if (isPalindrome(i, j)):
-                    ans = min(ans, dp(j+1) + 1)
-            return ans
-        
-        return dp(0) - 1
+        return solve(0)-1
