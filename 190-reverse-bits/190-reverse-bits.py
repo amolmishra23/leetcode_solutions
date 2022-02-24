16eb9c3a0bdf7ea2e99d963fca3f76b59a413a21
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-24 09:15:53 +0530

    Time: 44 ms (54.60%), Space: 13.8 MB (79.80%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,7 +1,19 @@
+# class Solution:
+#     def reverseBits(self, n: int) -> int:
+#         res = 0
+#         for _ in range(32):
+#             res = (res<<1) + (n&1)
+#             n>>=1
+#         return res
+
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = 0
-        for _ in range(32):
-            res = (res<<1) + (n&1)
-            n>>=1
-        return res
+        result = 0
+        
+        for i in range(32):
+            result <<= 1
+            result |= n&1
+            n >>= 1
+            
+        return result
+        
