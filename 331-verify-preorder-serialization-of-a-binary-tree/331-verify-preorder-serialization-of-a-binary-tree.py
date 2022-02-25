a4269e838acf7eafc657a1249a8ec0d698011335
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-25 11:16:26 +0530

    Time: 48 ms (53.68%), Space: 14 MB (72.82%) - LeetHub

---
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        """
+        normal nodes will:
+        1. occupy one place.
+        2. creates 2 slots, for 2 child nodes.
+        
+        Empty nodes will:
+        1. occupy one place. 
+        """
+        
+        p = preorder.split(",")
+        
+        # for the root node
+        slot = 1
+        
+        for node in p:
+            # if we have an empty slot and trying to insert a node means pre-order traversal is wrong. 
+            if slot==0: return False
+            
+            # for every # encountered by us, we will reduce node of slot by 1, as already used
+            if node=="#": slot-=1
+            
+            # for every normal number encountered, we will increase the slots by 1. As -1+2. 
+            else: slot+=1 # (Also same as -1+2)
+                
+        # in the end we should not have any empty slots. 
+        return slot==0
