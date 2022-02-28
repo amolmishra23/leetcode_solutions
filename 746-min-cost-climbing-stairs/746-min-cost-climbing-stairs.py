06fe8166b08579602a414f4799c2b4aaf3deb10e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-28 08:17:05 +0530

    Time: 71 ms (67.52%), Space: 16.7 MB (5.16%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+      @lru_cache(None)
+      def solve(idx):
+        if idx>=len(cost): return 0
+        
+        return cost[idx] + min(solve(idx+1), solve(idx+2))
+      
+      return min(solve(0), solve(1))
