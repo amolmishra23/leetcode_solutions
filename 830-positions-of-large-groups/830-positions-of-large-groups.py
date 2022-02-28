af302e4c1c6c5c7ba150ae9fb4d907834c5c1a33
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-02-28 08:07:41 +0530

    Time: 24 ms (89.92%), Space: 13.6 MB (51.26%) - LeetHub

---
+++ 830-positions-of-large-groups/830-positions-of-large-groups.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def largeGroupPositions(self, s):
+        """
+        :type s: str
+        :rtype: List[List[int]]
+        """
+        start, counter = 0, 1
+        res = []
+        n = len(s)
+        for i in range(n):
+            if i<n-1 and s[i]==s[i+1]: counter+=1
+            else:
+                if counter>=3: 
+                    res.append([start, i])
+                start, counter = i+1, 1
+        return res
