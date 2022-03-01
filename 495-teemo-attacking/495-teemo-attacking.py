40c5202eb9753b35832b9af5af55a1c4f1a87dad
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-01 19:08:47 +0530

    Time: 545 ms (8.44%), Space: 15.5 MB (45.05%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.py
+++ 495-teemo-attacking/495-teemo-attacking.py
@@ -5,6 +5,13 @@ class Solution:
         
         # but that there could be overlappings.
         # to overcome the overlappings, the trick is:
+        """
+        [1,4] with duration 2 means
+        2-(4-1) = 2-3 = -1. So we subtract 0, and no effetc
+        
+        [1,2] with duration 2 means
+        2-(2-1) = 2-1 = 1. So we subtract 1, and effect is there. 
+        """
         for i in range(1, len(ts)):
             res -= max(0, d-(ts[i]-ts[i-1]))
             
