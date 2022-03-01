993ddeabc8ad1679acab25aa09e711ba9c634c39
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-01 18:27:55 +0530

    Time: 49 ms (66.20%), Space: 14.3 MB (47.65%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dp = {}
+
+        def find(i, j):
+            if (i, j) not in dp:
+                if i == j:
+                    return nums[i]
+                dp[i,j] = max(nums[i]-find(i+1, j), nums[j]-find(i, j-1))
+            return dp[i,j]
+
+        return find(0, len(nums)-1) >= 0
