e0283ca941419d6a5ad7e378a34e27e26555f338
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-01 13:40:05 +0530

    Time: 44 ms (47.82%), Space: 13.8 MB (81.70%) - LeetHub

---
+++ 476-number-complement/476-number-complement.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        # return int(''.join(['1' if x=='0' else '0' for x in bin(num)[2:]]), 2)
+        # return int(''.join(['1' if x=='0' else '0' for x in bin(num)[2:]]), 2)
+        return num ^ ((1<<num.bit_length())-1)
