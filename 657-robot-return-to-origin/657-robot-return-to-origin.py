2157ed086b684e45d38aa8ef2abe038988a24fa6
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-02 19:11:52 +0530

    Time: 91 ms (37.19%), Space: 14.1 MB (67.08%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        count = Counter(moves); return count["U"]==count["D"] and count["L"]==count["R"]
+        
