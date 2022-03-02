882277693579f9b52ffac24821801ef9ed2e1843
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-02 11:37:31 +0530

    Time: 568 ms (68.35%), Space: 14.3 MB (71.67%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -3,6 +3,14 @@ import heapq
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        """
+        For choosing which task to do, we can put in a counter. 
+        And store it in max_heap, which gives tax with max count remaining. 
+        If heap doesnt have any more tasks to do in the same stretch, we can have that many empty slots also in between. 
+        
+        Meanwhile dont add them back instantly to heap, until the time is period. 
+        So we could keep storing in a temp list. 
+        """
         count = Counter(tasks)
         heap = []
         res = 0
