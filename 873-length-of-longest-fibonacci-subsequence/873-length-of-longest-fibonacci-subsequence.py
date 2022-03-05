14ffef1b62a3c6cbe423cce1ef44d16993167d0f
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 15:18:03 +0530

    Time: 2286 ms (60.89%), Space: 14.9 MB (42.67%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.py
@@ -5,10 +5,14 @@ class Solution:
         """
         n = len(A)
         lookup = {}
+        
         for i in range(len(A)):
             lookup[A[i]] = i
+        
         longest = collections.defaultdict(lambda: 2)
+        
         maxLength = 2
+        
         for i in range(len(A)):
             a = A[i]
             for j in range(i + 1, len(A)):
@@ -17,4 +21,5 @@ class Solution:
                 if c in lookup:
                     longest[(j, lookup[c])] = longest[(i, j)] + 1
                     maxLength = max(longest[(j, lookup[c])], maxLength)
+        
         return maxLength if maxLength != 2 else 0
