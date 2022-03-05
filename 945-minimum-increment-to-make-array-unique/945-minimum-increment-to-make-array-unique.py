c8c723ea9d0d1019377f72d29e4c6ceceaead24a
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 19:21:33 +0530

    Time: 1123 ms (60.24%), Space: 28.3 MB (71.49%) - LeetHub

--- 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
@@ -4,6 +4,11 @@ class Solution:
         target, result = 0, 0
         
         for i in range(len(A)):
+            """
+            goal is to make all elements of array unique here
+            so we sort the array and then try next elem to be bigger than target elem
+            Else we need to add the diff in the result
+            """
             if A[i]>=target: target=A[i]+1
             else:
                 result += target-A[i]
