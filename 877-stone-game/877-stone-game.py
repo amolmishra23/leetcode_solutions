2cfef6a829fb399291207cc7a8efddc813177b61
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 15:30:02 +0530

    Time: 616 ms (37.50%), Space: 125.1 MB (17.82%) - LeetHub

---
+++ 877-stone-game/877-stone-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        @functools.lru_cache(None)
+        def solve(i, j):
+            if i>j: return 0
+            return max(piles[i]-solve(i+1, j), piles[j]-solve(i, j-1))
+        
+        return solve(0, len(piles)-1)
