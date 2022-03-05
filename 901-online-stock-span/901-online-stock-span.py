b061c2420c09a413d4b37d48ffe1c64cb43ea23f
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 16:41:02 +0530

    Time: 642 ms (32.77%), Space: 19.6 MB (34.86%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.py
@@ -0,0 +1,22 @@
+class StockSpanner:
+    """
+    Classic example of nearest greater element to the left.
+    Combined with dp approach, so that we dont have to traverse the entire array. 
+    """
+    def __init__(self):
+        self.stk = []
+
+    def next(self, price: int) -> int:
+        res = 1
+        
+        while self.stk and self.stk[-1][0]<=price: 
+            res += self.stk.pop()[1]
+        
+        self.stk.append((price, res))
+    
+        return res
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
