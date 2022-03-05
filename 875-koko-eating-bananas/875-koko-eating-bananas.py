d12aee5951fc6970490dfd6294e1727f140b897c
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 15:27:43 +0530

    Time: 937 ms (13.16%), Space: 15.6 MB (57.34%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,5 +1,11 @@
 class Solution:
+    """
+    https://www.youtube.com/watch?v=UmuW0C4eFYg
+    """
     def solve(self, k, piles, h):
+        # finding total number of hours it takes
+        # if we restrict to eating k banana maximum per hour
+        # lets say we have 7 banana. At speed of 3. We will take 3 hrs. 
         total = 0
         
         for i in range(len(piles)):
@@ -11,6 +17,8 @@ class Solution:
         
         
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # our aim is to eat banana's nearest to guard return time. 8 hours. 
+        # We can eat anywhere between 1 and maximum number of banana's
         l, r = 1, max(piles)
         
         while l<r:
