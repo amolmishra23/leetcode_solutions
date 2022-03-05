e7f1475ac27a68b56a42c198c751e1241bd0bb91
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-05 20:20:24 +0530

    Time: 75 ms (37.93%), Space: 14.2 MB (68.23%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = {}
+        def solve(days, costs, idx):
+            """
+            This function's job is to return what is cheapest from idx to nth index. 
+            We go in a top down fashion, and try all the possibilities, and come to conclusion. 
+            """
+            if idx in dp: return dp[idx]
+            if idx>=len(days): return 0
+            
+            # if we buy 1 day pass, we need to have pass from next index onwards. 
+            cost_1 = costs[0] + solve(days, costs, idx+1)
+            
+            # finding until which index are we secured, if we buy a 7 day pass
+            # because the days are not continuous, need to find exact index, until which are are good to not buy any pass. 
+            i = idx
+            while i<len(days) and days[i]<days[idx]+7: i+=1
+            cost_7 = costs[1] + solve(days, costs, i)
+            
+            # finding until which index are we secured, if we buy a 30 day pass
+            i = idx
+            while i<len(days) and days[i]<days[idx]+30: i+=1
+            cost_30 = costs[2] + solve(days, costs, i)
+            
+            dp[idx] = min(cost_1, cost_7, cost_30)
+            return dp[idx]
+        
+        return solve(days, costs, 0)
