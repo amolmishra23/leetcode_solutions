32f6fdd7f7a731a2034026aa8a6c7a616e8d253a
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-06 17:38:55 +0530

    Time: 805 ms (51.27%), Space: 110.4 MB (6.04%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        jobs = sorted(list(zip(startTime, endTime, profit)))
+        startTime = [jobs[i][0] for i in range(n)]
+
+        @lru_cache(None)
+        def dp(i):
+            if i == n: return 0
+            # not doing the job
+            ans = dp(i + 1)  # Choice 1: Don't pick
+
+            # doing this job, and finding the next best job to do
+            j = bisect_left(startTime, jobs[i][1])
+            # now adding profit of the current task, and doing it again from dp(j), whose start doesnt collide with our end
+            ans = max(ans, dp(j) + jobs[i][2])  # Choice 2: Pick
+            return ans
+
+        return dp(0)
