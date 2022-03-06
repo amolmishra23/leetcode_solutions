5718278e0aed9c1b85107b2cbfa68b6bbc58d1af
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-06 16:34:26 +0530

    Time: 3617 ms (60.87%), Space: 14.9 MB (66.18%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,50 +1,23 @@
-from sortedcontainers import SortedList
-
 class Solution:
-  def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
-    m, n = len(matrix), len(matrix[0])
-
-    ans = -math.inf
-
-    """
-    We add prefix sum row wise also. 
-    Doing it in the order [0,1,2,3], [1,2,3], [2,3], [3] rows. 
-    """
-    for r1 in range(m):
-      arr = [0]*n
-      # Expanding step by step into all possible subarrays
-      for r2 in range(r1, m):
-        for c in range(n):
-          arr[c] += matrix[r2][c]
-        ans = max(ans, self.solve(arr, k))
-
-    return ans
-      
-  def solve(self, arr, k):
-    """
-    Logic is, we keep finding the prefix sum. 
-    Now we need sum <=k. 
-    So what do we subtract from the running prefix sum to get best possible <=k. 
-    For this, all the prefix sums we store it in sorted list. 
-    Return the max possible answer <= k. 
-    """
-    prefix_sum = 0
-    seen = SortedList([0])
-    ans = -math.inf
-    for i in range(len(arr)):
-      prefix_sum += arr[i]
-      prev_sum = self.ceiling(seen, prefix_sum-k)
-      if prev_sum!=None:
-        ans = max(ans, prefix_sum-prev_sum)
-      seen.add(prefix_sum)
-    return ans
-      
-  def ceiling(self, arr, key):
-    """
-    bisect_left returns the first index where key can be inserted 
-    that array will still be sorted. 
-    If this index, contains an element, is next bigger element after key. 
-    """
-    idx = arr.bisect_left(key)
-    if idx<len(arr): return arr[idx]
-    return None
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        def maxSumSubarray(arr):
+            sub_s_max = float('-inf')
+            s_curr = 0
+            prefix_sums = [float('inf')]
+            for x in arr:
+                bisect.insort(prefix_sums, s_curr)
+                s_curr += x
+                i = bisect.bisect_left(prefix_sums, s_curr - k)
+                sub_s_max = max(sub_s_max, s_curr - prefix_sums[i])
+            return sub_s_max
+        
+        m, n = len(matrix), len(matrix[0])
+        for x in range(m):
+            for y in range(n - 1):
+                matrix[x][y+1] += matrix[x][y]
+        res = float('-inf')
+        for y1 in range(n):
+            for y2 in range(y1, n):
+                arr = [matrix[x][y2] - (matrix[x][y1-1] if y1 > 0 else 0) for x in range(m)]
+                res = max(res, maxSumSubarray(arr))
+        return res
