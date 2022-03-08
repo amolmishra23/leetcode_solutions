ad4e5431301118d8fc61681cf3c9c6a762a6a1a0
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-08 09:58:21 +0530

    Time: 599 ms (9.42%), Space: 18.6 MB (63.60%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
@@ -1,5 +1,16 @@
 class Solution:
     def subarraysDivByK(self, A: List[int], K: int) -> int:
+        """
+        Very easy question
+        https://www.youtube.com/watch?v=kiHpKTfovRY
+        To check divisibility all we bother is, mod with k, should be 0
+        Or if we got mod as 2, after sometime if we again 2 means. Elements in between%k=0. 
+        So this subarray is divisible by k
+        
+        If we get mod as 2 multiple times, to find number of subarrays. An easy trick is, keep adding whatever subarray count is currently.
+        Hence we first add to res, using preSums.get(s, 0)
+        Finally we cache the count of s encountered. 
+        """
         preSums = {0: 1}
         s = 0
         res = 0
