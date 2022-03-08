a7fd037e24f83df337457b874b58febd23050e1e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-08 10:36:05 +0530

    Time: 2528 ms (83.25%), Space: 14.7 MB (86.70%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
@@ -5,6 +5,11 @@ class Solution:
             s_curr = 0
             prefix_sums = [float('inf')]
             for x in arr:
+                """
+                Intent here is, to insert the curr running sum in the array
+                Now find the most optimal value to subtract, so as to get k
+                Whatever is the nearest value to k, we keep updating in subarray max
+                """
                 bisect.insort(prefix_sums, s_curr)
                 s_curr += x
                 i = bisect.bisect_left(prefix_sums, s_curr - k)
@@ -12,10 +17,34 @@ class Solution:
             return sub_s_max
         
         m, n = len(matrix), len(matrix[0])
+        
+        # col wise we do prefix sum sort of thing.
+        """
+        First of all if our matrix is 
+        1 1 1 1
+        1 1 1 1
+        1 1 1 1
+        1 1 1 1
+        We calculate the pre_sum column wise, and convert our matrix to
+        1 2 3 4
+        1 2 3 4
+        1 2 3 4
+        1 2 3 4
+        """
         for x in range(m):
             for y in range(n - 1):
                 matrix[x][y+1] += matrix[x][y]
         res = float('-inf')
+        
+        """
+         Basic idea is, the sum is calculated in the following sequence
+        1. [0..0], [0..1], [0..2], [0..3]
+        2. [1..1], [1..2], [1..3]
+        3. [2..2], [2..3]
+        4. [3..3]
+        
+        This above was just columns, We also need to do across rows. Hence another iteration too. 
+        """
         for y1 in range(n):
             for y2 in range(y1, n):
                 arr = [matrix[x][y2] - (matrix[x][y1-1] if y1 > 0 else 0) for x in range(m)]
