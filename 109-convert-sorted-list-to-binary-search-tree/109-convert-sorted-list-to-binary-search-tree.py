c8a6d813e54018ddc60d4a5464ae198ea0266040
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-08 18:19:42 +0530

    Time: 373 ms (68.76%), Space: 20.3 MB (54.32%) - LeetHub

---
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+
+    def findSize(self, head):
+        ptr = head
+        c = 0
+        while ptr:
+            ptr = ptr.next
+            c += 1
+        return c
+
+
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+
+        # Get the size of the linked list first
+        size = self.findSize(head)
+
+        # Recursively form a BST out of linked list from l --> r
+        def convert(l, r):
+            nonlocal head
+
+            # Invalid case
+            if l > r:
+                return None
+
+            mid = (l + r) // 2
+
+            # First step of simulated inorder traversal. Recursively form
+            # the left half
+            left = convert(l, mid - 1)
+
+            # Once left half is traversed, process the current node
+            node = TreeNode(head.val)   
+            node.left = left
+
+            # Maintain the invariance mentioned in the algorithm
+            head = head.next
+
+            # Recurse on the right hand side and form BST out of them
+            node.right = convert(mid + 1, r)
+            return node
+        
+        return convert(0, size - 1)
