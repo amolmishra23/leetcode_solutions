0a0633cc1cc0b243c8db9cf4976e4db2aa26095e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-15 08:28:26 +0530

    Time: 67 ms (51.81%), Space: 14.3 MB (17.24%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -1,5 +1,16 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
+        """
+        1 => 1
+        2 => 11
+        3 => 21
+        4 => 1211
+        
+        To get the term for 4, we read the previous term 3.
+        And go adding number of elements from previous term. 
+        As we had 21. Means one 2, and one 1.
+        Hence the result is 1211
+        """
         def solve(seq):
             i, new_seq = 0, []
             while i<len(seq):
