7bd87620df37c3ed0051b1b66f1d88879dfa50ab
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-22 06:13:31 +0530

    Time: 59 ms (89.86%), Space: 14.9 MB (94.47%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -1,4 +1,14 @@
 class Solution:
+    """
+    Goal here is, to create "n" length string with sum total as "k"
+    a has 1, b has 2, z has 26. In sum.
+    So in best case possible if n==k, we can just send "a"*n. 
+    If thats not the case, we try to add as many "z" as possible from the end. 
+    If z is getting more, we find the mod, and add the character which is mod value. 
+    In the end our response is going to be 'a'*(n-z-1) + special_char + 'z'*z
+    
+    n-z-1 because, we subtract number of z added. And the one special character added. 
+    """
     def getSmallestString(self, n: int, k: int) -> str:
         
         # assume every char is an 'a' total = new k
