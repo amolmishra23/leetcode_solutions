fcff61aa8da432d55d4dec26be98a2a247cdea3a
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-03-27 08:49:06 +0530

    Time: 44 ms (59.62%), Space: 14 MB (28.82%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -1,20 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-  def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-    if not root:
-      return []
-
-    ans = []
-    q = deque([root])
-
-    while q:
-      size = len(q)
-      for i in range(size):
-        root = q.popleft()
-        if i == size - 1:
-          ans.append(root.val)
-        if root.left:
-          q.append(root.left)
-        if root.right:
-          q.append(root.right)
-
-    return ans
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        self.ans = []
+        
+        def dfs(root, depth):
+            if root == None: return
+            if depth == len(self.ans):  # When we meet this `depth` for the first time, let's add the first node as the right side most node.
+                self.ans.append(root.val)
+            dfs(root.right, depth + 1)  # Go right side first
+            dfs(root.left, depth + 1)
+            
+        dfs(root, 0)
+        return self.ans
