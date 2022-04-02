8546fcfc37b8bc994772a72358d29134ef080f2d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-02 21:16:34 +0530

    Time: 1124 ms (34.49%), Space: 15.4 MB (31.73%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.py
@@ -0,0 +1,25 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends: return -1
+        
+        def get_neigh(node):
+            res = []
+            for i in range(4):
+                res.append(node[:i] + str((int(node[i])+1)%10) + node[i+1:])
+                res.append(node[:i] + str((int(node[i])-1+10)%10) + node[i+1:])
+                
+            return res
+        
+        q = deque([("0000", 0)])
+        visited = set(deadends)
+        
+        while q:
+            node, level = q.popleft()
+            if node==target: return level
+            for neigh in get_neigh(node):
+                if neigh not in visited:
+                    visited.add(neigh)
+                    q.append((neigh, level+1))
+                    
+        return -1
+        
