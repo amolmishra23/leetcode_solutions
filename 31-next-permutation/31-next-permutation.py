f3e5d1bebf0027df6ca9b497157d08ebdf9f1091
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-03 06:42:54 +0530

    Time: 49 ms (72.04%), Space: 13.9 MB (79.00%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -2,27 +2,21 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        [6,2,1,5,4,3,0]
         """
-        k, l = -1, 0
-        n = len(nums)
-# need to find the first number before start of the decreasing sequence
-# Example [6, 2, 1, 5, 4, 3, 0]
-# So 1 is the number found.
-# In strictly decreasing sequence, 3 is the first number bigger than 1. So exchange it. 
-# [6, 2, 3, 5, 4, 1, 0]
-# Now reverse the remaining array. [6, 2, 3, 0, 1, 4, 0], as it is the first permutation, Return the result
+        n, k = len(nums), None
         for i in range(n-2, -1, -1):
             if nums[i]<nums[i+1]:
                 k = i
                 break
         else:
             nums.reverse()
-            return
-    
-        for i in range(n-1, k, -1):
-            if nums[i]>nums[k]:
-                l = i
-                break
-        nums[k], nums[l] = nums[l], nums[k]
+            return 
+        
+        j = None
+        for j in range(n-1, -1, -1):
+            if nums[j]>nums[i]: break
+        
+        nums[k], nums[j] = nums[j], nums[k]
         nums[k+1:] = nums[:k:-1]
         return nums
