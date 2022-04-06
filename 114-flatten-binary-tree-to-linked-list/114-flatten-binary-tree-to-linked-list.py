a5912b801fef362850237490b36ab903b4e1a42b
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-06 19:53:12 +0530

    Time: 55 ms (47.59%), Space: 15.3 MB (11.02%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.prev = None
+            
+    def flatten(self, root: TreeNode) -> None:
+        """
+        The logic here is to do a reverse post order traversal.
+        traverse to the right most. 
+        traverse to the left.
+        
+        Now set right child as self.prev. left as none. 
+        And finally cache the current value as self.prev for future purposes. 
+        """
+        if root is None: return None
+        
+        self.flatten(root.right)
+        self.flatten(root.left)
+        
+        root.right = self.prev
+        root.left = None
+        self.prev = root
+            
+            
+            
