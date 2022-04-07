9f1ac2b124a09c99c43fd064f0bc51631341eb11
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 20:15:15 +0530

    Time: 88 ms (90.08%), Space: 14.4 MB (38.24%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,19 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         res = nums[0]
-        max_, min_ = res, res
+        mx, mn = res, res
         
         for i in range(1, len(nums)):
-            # if curr num is -ve, max can be obtained by multiplying with most min_ number. 
-            # hence this swap
-            if nums[i]<0: max_, min_ = min_, max_
-                
-            # checking like kadane algorithm. if max is by continuing the streak or starting from this index
-            max_ = max(nums[i], max_*nums[i])
-            # because of -ve numbers occurence, we also have to keep finding the -ve most elem as per kadane algo
-            min_ = min(nums[i], min_*nums[i])
-            
-            # keep updating the result
-            res = max(res, max_)
-            
+            if nums[i]<0: mx, mn = mn, mx
+            mx = max(nums[i], mx*nums[i])
+            mn = min(nums[i], mn*nums[i])
+            res = max(res, mx)
+        
         return res
