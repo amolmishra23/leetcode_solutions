f618304292427e334c31fd6684aecaf749ea7343
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 21:59:00 +0530

    Time: 44 ms (82.42%), Space: 14.2 MB (9.67%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,22 +1,14 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        """
-        Aim of the problem is to calculate how many possible permutations can generate a target sum?
-        We go in a deep recursion tree, to see all permutations which can bring us the target sum
-        Basically if we have num 3, and target 5. Total permutations is, 3+num of ways to get target(2)
-        Hence we solve that over all the numbers and return the result. 
-        """
-        def solve(nums, target):
-            if dp[target] is not None: return dp[target]
-
-            res=0
-            for num in nums:
-                if num<=target: res+=solve(nums, target-num)
-
-            dp[target]=res
+        @lru_cache(None)
+        def solve(target):
+            if target==0: return 1
+            
+            res = 0
+            for n in nums:
+                if n<=target: 
+                    res += solve(target-n)
+                    
             return res
-
-        dp = [None]*(target+1)
-        dp[0]=1
-        solve(nums, target)
-        return dp[target]
+        
+        return solve(target)
