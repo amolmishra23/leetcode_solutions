8cb23348e1209a2ef2ba0a3d5b90d255ab2344c1
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 21:19:53 +0530

    Time: 40 ms (97.29%), Space: 14.3 MB (15.94%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,17 +1,21 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        
         @lru_cache(None)
         def dp(i, j):
-            if j == len(p):  # If `j` reach end of pattern `p`
-                return i == len(s)  # Then it fully matches if and only if `i` reach end of string `s`
-				
-            if j+1 < len(p) and p[j+1] == '*':
-                ans = dp(i, j+2) # match zero chars
-                if i < len(s) and (s[i] == p[j] or p[j] == '.'):
-                    ans = ans or dp(i+1, j) # match 1 char, skip 1 char in s, don't skip in p since it can march zero or match more characters
-                return ans
-            if i < len(s) and (s[i] == p[j] or p[j] == '.'):
-                return dp(i+1, j+1) # match 1 char, skip 1 char in both s and p
+            if j==n: return i==m
+            
+            if j+1<n and p[j+1]=="*":
+                res = dp(i, j+2)
+                if not res:
+                    if i<m and (s[i]==p[j] or p[j]=="."):
+                        res = dp(i+1, j)
+                return res
+            
+            if i<m and (s[i]==p[j] or p[j]=="."): return dp(i+1, j+1)
+            
             return False
         
         return dp(0, 0)
+            
