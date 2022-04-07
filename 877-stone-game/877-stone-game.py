296b44ce41fb456f07c66dd2fab5f97268ef53e3
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 12:20:39 +0530

    Time: 592 ms (40.17%), Space: 125 MB (11.45%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,16 +1,8 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        @lru_cache(None)
-        def dp(left, right):
-            if left > right: return (0, 0)
-            
-            pickLeft = dp(left+1, right)
-            pickRight = dp(left, right - 1)
-            
-            if piles[left] + pickLeft[1] > piles[right] + pickRight[1]:  # If the left choice has higher score than the right choice
-                return piles[left] + pickLeft[1], pickLeft[0]  # then pick left
-            
-            return piles[right] + pickRight[1], pickRight[0]  # else pick right
+        @functools.lru_cache(None)
+        def solve(i, j):
+            if i>j: return 0
+            return max(piles[i]-solve(i+1, j), piles[j]-solve(i, j-1))
         
-        alexScore, leeScore = dp(0, len(piles) - 1)
-        return alexScore > leeScore
+        return solve(0, len(piles)-1)
