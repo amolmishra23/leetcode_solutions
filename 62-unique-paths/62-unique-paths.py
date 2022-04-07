47deca5ada9995cfa59ec8f43ccff222139a015d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 21:37:30 +0530

    Time: 36 ms (77.57%), Space: 14.2 MB (7.00%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,13 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @functools.lru_cache(None)
-        def solve(m, n):
-            if m==x-1 and n==y-1: return 1
+        @lru_cache(None)
+        def solve(x, y):
+            if x==m-1 and y==n-1: return 1
             
-            if not 0<=m<x or not 0<=n<y: return 0
+            if not 0<=x<m or not 0<=y<n: return 0
             
-            return solve(m+1, n) + solve(m, n+1)
+            return solve(x+1, y) + solve(x, y+1)
         
-        x, y = m, n
-        return solve(0,0)
-            
+        return solve(0, 0)
