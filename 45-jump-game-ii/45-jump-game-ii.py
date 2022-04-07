2ca13f81c5e7794380578cabbe4008f6e9978b08
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 21:34:20 +0530

    Time: 11809 ms (11.28%), Space: 30.6 MB (5.01%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        memo = {}
-        N = len(nums)
-
-        def dp(index):
-            if index >= N - 1:
-                return 0
-
-            if index in memo:
-                return memo[index]
-
-            min_jump = math.inf
-            for i in range(1, nums[index] + 1):
-                min_jump = min(min_jump, 1 + dp(index + i))
-                memo[index] = min_jump
-
-            return min_jump
-        return dp(0)
+        n = len(nums)
+        
+        @lru_cache(None)
+        def solve(idx):
+            if idx >= n-1: return 0
+            
+            res = float('inf')
+            for i in range(1, nums[idx]+1):
+                res = min(res, 1+solve(idx+i))
+                
+            return res
+        
+        return solve(0)
