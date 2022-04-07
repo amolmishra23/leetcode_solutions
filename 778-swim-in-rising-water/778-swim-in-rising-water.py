d8e02e6f920795e1f29bdcad97e1eb6fbde6ba2e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 08:31:40 +0530

    Time: 206 ms (34.45%), Space: 14.6 MB (82.75%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.py
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -1,38 +1,17 @@
-class UF:
-    def __init__(self, n):
-        self.parents = list(range(n))
-        self.size = [1]*n
-        
-    def find(self, x):
-        if self.parents[x]==x: return x
-        self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-    
-    def union(self, x, y):
-        par_x, par_y = map(self.find, [x,y])
-        if par_x==par_y: return 
-        small, big = sorted([par_x, par_y], key = lambda x: self.size[x])
-        self.parents[small] = big
-        self.size[big] += self.size[small]
-
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+        queue = [(grid[0][0], 0, 0)]
+        res = float('-inf')
         n = len(grid)
-        uf = UF(n**2)
-        rev_map = [None]*(n**2)
+        seen = set()
         
-        for i in range(n):
-            for j in range(n):
-                rev_map[grid[i][j]] = (i, j)
         
-        allowed = [(-1,0),(1,0),(0,1),(0,-1)]
-        
-        for i in range(n**2):
-            x,y = rev_map[i]
-            for dx,dy in allowed:
-                nx,ny = x+dx, y+dy
-                if 0<=nx<n and 0<=ny<n and grid[nx][ny]<=i:
-                    uf.union(x*n+y, nx*n+ny)
-                    if uf.find(0)==uf.find(n**2-1): return i
-                    
-        return n**2-1                    
+        while queue:
+            c, x, y = heapq.heappop(queue)
+            res = max(res, c)
+            if x==y==n-1: return res
+            
+            for i, j in ((x+1,y), (x-1,y),(x,y+1),(x,y-1)):
+                if 0<=i<n and 0<=j<n and (i,j) not in seen:
+                    seen.add((i,j))
+                    heapq.heappush(queue, (grid[i][j], i, j))
