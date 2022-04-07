dd2e4258aafdddd138eb9ae35d27d6cd430f3798
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 19:19:29 +0530

    Time: 49 ms (44.48%), Space: 14.3 MB (11.92%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,20 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @functools.lru_cache(None)
-        def solve(s, i):
-            # if we reached till end of the string, surely its a valid permutation
-            # and return 1 so that, it adds up as a valid permutation
-            if i>=len(s): return 1
+        n = len(s)
+        
+        @lru_cache(None)
+        def solve(idx):
+            if idx>=n: return 1
             
-            # checking from the current char, and getting all possible valid permutations
-            res, c = 0, s[i]
+            res, c = 0, s[idx]
             
-            # if char is bigger than 0, chance of a valid permutation
-            if int(c)>0: res += solve(s, i+1)
+            if int(c)>0: res += solve(idx+1)
                 
-            # if char is between 10-26, another chance of a permutation
-            if i+1<len(s) and 10<=int(s[i:i+2])<=26: res += solve(s, i+2)
-                
+            if idx+1 < n and 10<=int(s[idx:idx+2])<=26: res += solve(idx+2)
+                    
             return res
         
-        return solve(s, 0)
+        return solve(0)
