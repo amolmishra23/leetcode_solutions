f770d5ffce80bb4c1863184b36018b8e4e6b1a0d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-07 17:54:18 +0530

    Time: 1107 ms (15.55%), Space: 290.5 MB (5.36%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,13 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @lru_cache(None)
-        def coin_change(idx, target):
-            if target==0: return 1
-            
-            if target<0: return 0
-            
-            if idx>=len(coins): return 0
-            
-            return coin_change(idx, target-coins[idx])+coin_change(idx+1, target)
+        n = len(coins)
         
-        return coin_change(0, amount)
+        @lru_cache(None)
+        def solve(idx, target):
+            if target==0: return 1
+            if target<0: return 0
+            if idx>=n: return 0
+            return solve(idx, target-coins[idx]) + solve(idx+1, target)
+        
+        return solve(0, amount)
