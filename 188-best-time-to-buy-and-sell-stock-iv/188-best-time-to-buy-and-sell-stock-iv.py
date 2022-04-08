507f97caf8baff718e7760980110b9a14ecf5ba2
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-08 08:04:32 +0530

    Time: 358 ms (14.09%), Space: 52 MB (15.78%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,19 +1,21 @@
 class Solution:
-    def maxProfit(self, k: int, arr: List[int]) -> int:
-        @lru_cache(None)
-        def solve(idx, can_buy, k):
-          if idx==len(arr) or k==0:
-            return 0
-          
-          if can_buy:
-            return max(
-              solve(idx+1, False, k) - arr[idx],
-              solve(idx+1, True, k)
-            )
-          else:
-            return max(
-              solve(idx+1, True, k-1)+arr[idx],
-              solve(idx+1, False, k)
-            )
-          
-        return solve(0, True, k)
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        dp = {}
+        
+        def dfs(i, buying, k):
+            if i>=len(prices) or k==0: return 0
+            
+            if (i, buying, k) in dp: return dp[(i, buying, k)]
+            
+            if buying:
+                buy = -prices[i] + dfs(i+1, not buying, k)
+                cooldown = dfs(i+1, buying, k)
+                dp[(i, buying, k)] = max(buy, cooldown)
+            else:
+                sell = prices[i] + dfs(i+1, not buying, k-1)
+                cooldown = dfs(i+1, buying, k)
+                dp[(i, buying, k)] = max(sell, cooldown)
+                
+            return dp[(i, buying, k)]
+        
+        return dfs(0, True, k)
