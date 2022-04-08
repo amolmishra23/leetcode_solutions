fbea54c9de3064d956ee51bfa6a58586e8154f77
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-08 08:21:43 +0530

    Time: 51 ms (44.67%), Space: 13.8 MB (66.98%) - LeetHub

--- 343-integer-break/343-integer-break.py
+++ 343-integer-break/343-integer-break.py
@@ -1,19 +1,19 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        if n<=2: return 1
+        dp = {1:1}
         
-        dp = [0]*(n+1)
-        # Because even for dp[2], best way to split is 1+1, and 1*1=1
-        dp[1], dp[2] = 1, 1
-        
-        for i in range(3, n+1):
-            for j in range(1, i):
-                """
-                The reason for this being, in case of 5 we can either write it as 2+3 or as 2+2+1
-                To do as 2+3 => j*(i-j)
-                To do as 2+2+1 => j*dp[i-j], which inturn finds the best answer for 3. 
-                Hence its done that way!
-                """
-                dp[i] = max(dp[i], max(j*dp[i-j], j*(i-j)))
+        def dfs(num):
+            if num in dp: return dp[num]
+            
+            # in subproblem no need to break the number
+            # in actual problem we definitely need to break, hence this condition
+            res=0 if num==n else num
+            
+            for i in range(1, num):
+                val = dfs(i) * dfs(num-i)
+                res = max(res, val)
                 
-        return dp[n]
+            dp[num] = res
+            return dp[num]
+        
+        return dfs(n)
