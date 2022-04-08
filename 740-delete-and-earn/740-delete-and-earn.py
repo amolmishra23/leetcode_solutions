7463a76daf307121edafd433a6435cbe9c84b195
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-08 09:00:16 +0530

    Time: 108 ms (34.05%), Space: 28.8 MB (6.30%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,33 +1,18 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         n = len(nums)
-        arr = []
-        cnt = 0
-        lastNum = 0  # Since nums[i] >= 1, lastNum = 0 is a valid choice to out of range value of nums
-
-        count = Counter(nums)
-        for x,y in sorted(count.items()):
-            arr.append([x, y])
-            
-        print(arr)
+        
+        arr = [(num, count) for num, count in sorted(Counter(nums).items())]
         
         @lru_cache(None)
         def dp(i, lastPicked):
-            if i == len(arr): return 0
-            # Dont pick i
-            ans = dp(i+1, False)
+            if i>=len(arr): return 0
             
-            # Pick i
-            """
-            Only conditions to be selected. 
-            1. if we are at start index.
-            2. if last picked and last element was arr[i]-1. Then we dont pick.
+            res = dp(i+1, False)
             
-            Else we try to pick, and to result we try adding arr[i][0]*arr[i][1]
-            """
-            if i == 0 or not (lastPicked and arr[i-1][0] == arr[i][0]-1):
-                ans = max(ans, dp(i+1, True) + arr[i][0] * arr[i][1])
-            
-            return ans
+            if i==0 or (lastPicked and arr[i-1][0]+1==arr[i][0])==False:
+                res = max(res, arr[i][0]*arr[i][1] + dp(i+1, True))
+                
+            return res
         
         return dp(0, False)
