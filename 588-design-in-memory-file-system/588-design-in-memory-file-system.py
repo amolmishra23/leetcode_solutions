a66d4ff44013c947d5fd1b0e6b5ac5816771695d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 07:28:45 +0530

    Time: 69 ms (68.84%), Space: 14.5 MB (64.81%) - LeetHub

---
+++ 588-design-in-memory-file-system/588-design-in-memory-file-system.py
@@ -0,0 +1,54 @@
+class FileSystem:
+
+    def __init__(self):
+        self.trie = {}
+
+    def ls(self, path: str) -> List[str]:
+        if len(path)==1: return sorted(self.trie.keys())
+        
+        path = path.split("/")
+        node = self.trie
+        
+        for p in path[1:]:
+            node = node.setdefault(p, {})
+            
+        if type(node)==str: return [path[-1]]
+        
+        return sorted(node.keys())
+
+    def mkdir(self, path: str) -> None:
+        path = path.split("/")
+        node = self.trie
+        
+        for p in path[1:]:
+            node = node.setdefault(p, {})
+
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        path = filePath.split("/")
+        file = path[-1]
+        node = self.trie
+        
+        for p in path[1:-1]:
+            node = node.setdefault(p, {})
+            
+        if file not in node:
+            node[file] = content
+        else:
+            node[file] += content
+
+    def readContentFromFile(self, filePath: str) -> str:
+        path = filePath.split("/")
+        node = self.trie
+        
+        for p in path[1:-1]:
+            node = node.setdefault(p, {})
+            
+        return node[path[-1]]
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.ls(path)
+# obj.mkdir(path)
+# obj.addContentToFile(filePath,content)
+# param_4 = obj.readContentFromFile(filePath)
