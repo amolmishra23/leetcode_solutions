49373b2076cd1591c0f4e06bd8d4cda366010820
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 18:50:45 +0530

    Time: 91 ms (27.46%), Space: 15.6 MB (8.42%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,22 +1,17 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        # the trick to build smallest number
-        # We need to have a strictly increasing array
-        # anywhere we see a dip, we can remove that element. from left=>right
-        
         stk = []
-        i = 0
         
-        for i in range(len(num)):
-            if not stk: stk.append(num[i])
+        for n in num:
+            if not stk: stk.append(n)
             else:
-                while stk and stk[-1]>num[i] and k>0:
+                while stk and stk[-1]>n and k:
                     stk.pop()
                     k-=1
-                stk.append(num[i])
+                stk.append(n)
                 
-        while stk and k!=0:
+        while k:
             stk.pop()
             k-=1
-        
+            
         return "".join(stk).lstrip("0") or "0"
