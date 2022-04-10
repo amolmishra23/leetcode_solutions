4483649ddfb3f1f2542f0e577181927b12a73dad
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 18:42:42 +0530

    Time: 482 ms (68.11%), Space: 19.6 MB (42.64%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.py
+++ 901-online-stock-span/901-online-stock-span.py
@@ -1,21 +1,16 @@
 class StockSpanner:
-    """
-    Classic example of nearest greater element to the left.
-    Combined with dp approach, so that we dont have to traverse the entire array. 
-    """
+
     def __init__(self):
         self.stk = []
 
     def next(self, price: int) -> int:
         res = 1
         
-        # We can use the stack logic, but also need to cache number of elements we popped.
-        # So in future when we pop stuff, we can be sure of count of things. 
-        while self.stk and self.stk[-1][0]<=price: 
+        while self.stk and self.stk[-1][0]<=price:
             res += self.stk.pop()[1]
-        
+            
         self.stk.append((price, res))
-    
+        
         return res
 
 
