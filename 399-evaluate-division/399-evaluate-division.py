58c1798511fdea451d49a7d0d4f447ed3eaa84f6
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 06:42:37 +0530

    Time: 76 ms (5.23%), Space: 13.9 MB (95.68%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,37 +1,30 @@
-import collections
-
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        """
-        Basic dfs problem
-        We store all the connections and weights in dict form { a: { b: c}}. a is from node, b is to node, c is distance. 
-        Store it like (a,b,c) and (b,a,1/c)
-        We need to traverse from a to x via dfs, and find the result and return client. 
-        We have a visited set, which keeps track of dfs. And we find the result as chain multiplication and return to client. 
-        """
-        graph = collections.defaultdict(dict)
+        root = {}
         
-        for (x,y), val in zip(equations, values):
-            graph[x][y] = val
-            graph[y][x] = 1.0/val
+        def find(x):
+            p, xr = root.setdefault(x, (x,1.0))
+            if x!=p:
+                r, pr = find(p)
+                root[x] = (r, xr*pr)
+            return root[x]
         
-        def dfs(x, y, visited):
-            if x not in graph or y not in graph: return -1.0
+        def union(x, y, ratio):
+            px,xr,py,yr = *find(x), *find(y)
+            if not ratio:
+                return xr/yr if px==py else -1.0
+            if px!=py:
+                root[px] = (py, yr/xr*ratio)
+                
+        for (x,y),v in zip(equations, values): 
+            union(x, y, v)
             
-            if y in graph[x]: return graph[x][y]
-            
-            for k in graph[x]:
-                if k not in visited:
-                    visited.add(k)
-                    temp = dfs(k, y, visited)
-                    if temp==-1: continue
-                    else:
-                        return graph[x][k] * temp
-            
-            return -1.0
-                    
-        res = []
-        for query in queries:
-            res.append(dfs(query[0], query[1], set()))
-            
-        return res
+        # print(root)
+        # print(find("a"))
+        # print(find("b"))
+        # print(find("c"))
+        # print(union("a", "b", 0))
+        # print(union("b", "a", 0))
+        
+        return [union(x,y,0) if x in root and y in root else -1.0 for x,y in queries]
+    
