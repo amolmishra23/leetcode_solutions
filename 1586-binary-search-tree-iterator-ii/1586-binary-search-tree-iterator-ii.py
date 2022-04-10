2736a6714b3d0e72d4101ff6115d9001a43e8d93
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 09:26:42 +0530

    Time: 785 ms (12.41%), Space: 45.6 MB (62.04%) - LeetHub

---
+++ 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.last = root
+        self.stack, self.arr = [], []
+        self.pointer = -1
+
+    def hasNext(self) -> bool:
+        return self.stack or self.last or self.pointer < len(self.arr)-1       
+
+    def next(self) -> int:
+        self.pointer += 1
+        
+        if self.pointer == len(self.arr):
+            while self.last:
+                self.stack.append(self.last)
+                self.last = self.last.left
+            curr = self.stack.pop()
+            self.last = curr.right
+            self.arr.append(curr.val)
+            
+        return self.arr[self.pointer]
+
+    def hasPrev(self) -> bool:
+        return self.pointer >0
+
+    def prev(self) -> int:
+        self.pointer -= 1
+        return self.arr[self.pointer]
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.hasNext()
+# param_2 = obj.next()
+# param_3 = obj.hasPrev()
+# param_4 = obj.prev()
