29813b2f322a7c4bed9559f7ee77cd1260432bb8
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 11:43:46 +0530

    Time: 51 ms (43.02%), Space: 13.9 MB (43.48%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,10 +1,10 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stk = []
-        vals = path.split('/')
+        vals = path.split("/")
         
         for val in vals:
-            if val=='..' and stk: stk.pop()
-            elif (val not in ['.', '..', '']): stk.append(val)
+            if val==".." and stk: stk.pop()
+            elif val not in [".", "..", ""]: stk.append(val)
         
-        return '/'+'/'.join(stk)
+        return "/" + "/".join(stk)
