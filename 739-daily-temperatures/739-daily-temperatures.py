49477825a6424315309c6b853ed66c53283a5cae
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 12:30:26 +0530

    Time: 2176 ms (8.34%), Space: 25.5 MB (25.46%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,29 +1,23 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        def next_greater(arr):
-            res = [None]*len(arr)
-            stk = []
-            
-            # typical AV solution to find the next greater element
-            for i in range(len(arr)-1, -1, -1):
-                # pop out all the elements, until we dont have a greater element than curr element
-                # we pop even the equal element, as this element would hold a precedence for the next greater element. 
-                while stk and arr[stk[-1]]<=arr[i]: stk.pop()
-                
-                # result is the next greater element after this number
-                if stk:
-                    res[i] = stk[-1]
-                else:
-                    res[i] = 0
-                
-                # we append this element, as it could be a potential solution for numbers on the left
-                stk.append(i)
-            
-            # this hardwork we do because, we need not next greater index, but no of days, from current day
-            for i in range(len(res)):
-                res[i] = max(res[i]-i, 0)
-            
-            return res
+    def ngr(self, arr):
+        n, res = len(arr), [None]*len(arr)
+        stk = []
         
-        return next_greater(temperatures)
+        for i in range(n-1, -1, -1):
+            while stk and arr[stk[-1]]<=arr[i]: stk.pop()
+            res[i] = stk[-1] if stk else float('-inf')
+            stk.append(i)
             
+        return res
+    
+    def dngr(self, arr):
+        ngr = self.ngr(arr)
+        res = [None]*len(arr)
+        
+        for i in range(len(arr)):
+            res[i] = max(ngr[i]-i, 0)
+            
+        return res
+        
+    def dailyTemperatures(self, arr: List[int]) -> List[int]:
+        return self.dngr(arr)
