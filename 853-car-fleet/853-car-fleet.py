305f34fa9808139ed0f514c6335fa40f3b4c17ab
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-10 18:39:44 +0530

    Time: 1460 ms (31.27%), Space: 36.1 MB (75.69%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,23 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        """
-        Idea is, some set of cars will reach together as fleet to the target.
-        We need to find how many such cars are there.
+        stk = []
         
-        Logic is, we find the remaining time to reach destination. distance/speed formula.
-        If something which can reach faster than the currently closest, we make it that much slow. And make reach as fleet.
-        In the end number of such cars in the stack is the result. 
-        """
-        stack = []
-        # we will have strictly increasing stack
-        # if we midway find anything smaller than curr top in stack, we ignore adding it to stack
-        for pos, vel in sorted(zip(position, speed))[::-1]:
-            dist = target - pos
-            if not stack:
-                stack.append(dist / vel)
-            # remaining time to reach, is more than the top of stack. Only then push it
-            # else if it can reach faster than top of stack element, we ignore pushing to stack
-            # and finally return the stack count as fleet. 
-            elif dist / vel > stack[-1]:
-                stack.append(dist / vel)
-        return len(stack)
+        for pos, speed in sorted(zip(position, speed))[::-1]:
+            dist = target-pos
+            rem_time = dist/speed
+            if not stk:
+                stk.append(rem_time)
+            elif stk[-1]<rem_time:
+                stk.append(rem_time)
+            else:
+                continue
+                
+        return len(stk)
