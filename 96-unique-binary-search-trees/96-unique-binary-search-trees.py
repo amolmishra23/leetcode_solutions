a1f1ed332851fb30601ee6b2e5ae9e0ef8157c48
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-26 17:40:17 +0530

    Time: 37 ms (63.49%), Space: 13.9 MB (62.94%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,13 +1,10 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         G = [0]*(n+1)
-        
         G[0] = G[1] = 1
         
         for i in range(2, n+1):
-            # we can have both 1, 2 as roots
-            # if 1 is root, left side will have G[1-0] nodes and right side will have G[2-1] nodes. 
-            for j in range(1, i+1):
+            for j in range(i+1):
                 G[i] += G[j-1] * G[i-j]
                 
         return G[-1]
