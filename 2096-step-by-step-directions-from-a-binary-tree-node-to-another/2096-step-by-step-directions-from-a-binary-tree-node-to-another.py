7b86c0223cfcfaa3dfe9adc3b7b1ead713632d21
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-26 17:46:42 +0530

    Time: 770 ms (83.26%), Space: 137.2 MB (54.05%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,18 +6,23 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        def find(n: TreeNode, val: int, path: List[str]) -> bool:
-            if n.val == val:
-                return True
-            if n.left and find(n.left, val, path):
+        def find(root, target, path):
+            if root.val == target: return True
+            if root.left and find(root.left, target, path):
                 path += "L"
-            elif n.right and find(n.right, val, path):
+                return True
+            elif root.right and find(root.right, target, path):
                 path += "R"
-            return path
+                return True
+            return False
+        
         s, d = [], []
         find(root, startValue, s)
         find(root, destValue, d)
-        while len(s) and len(d) and s[-1] == d[-1]:
+        
+        while s and d and s[-1]==d[-1]:
             s.pop()
             d.pop()
-        return "".join("U" * len(s)) + "".join(reversed(d))
+            
+        return "".join("U"*len(s)) + "".join(reversed(d))
+        
