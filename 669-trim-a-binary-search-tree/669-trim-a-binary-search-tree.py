65ae5df485ed5a6ff041ad8c4a93d6221acb5441
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-27 11:35:18 +0530

    Time: 71 ms (8.82%), Space: 18 MB (38.34%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.py
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.py
@@ -12,12 +12,13 @@ class Solution:
         """
         if not root: return None
         
+        # these 2 conditions are basically for performing pruning
         if root.val > high:
             return self.trimBST(root.left, low, high)
-            
         if root.val < low:
             return self.trimBST(root.right, low, high)
         
+        # if the curr node is in limit, we recurse on the left and right sub-trees to delete if any nodes not in the range
         root.left = self.trimBST(root.left, low, high)
         root.right = self.trimBST(root.right, low, high)
         
