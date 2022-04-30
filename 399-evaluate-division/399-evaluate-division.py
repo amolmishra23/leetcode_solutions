c1a4a8226f5d56dbc2ee4cac0dd61f3ee885bb1f
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-04-30 10:11:55 +0530

    Time: 40 ms (59.50%), Space: 14 MB (60.05%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -2,29 +2,38 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         root = {}
         
+        """
+        equations = [["a","b"],["b","c"]], values = [2.0,3.0]
+        after 1st equation, we have par[a]=b. ratio[a]=2
+        after 2nd equation, we have par[b]=c, ratio[b]=3
+        
+        when we need to find from a to b, in union find way we go from a=>b and then b=>c
+        multiply the respective ratio's. 2*3. hence answer is 6.
+        
+        As we do find, we also do path compression, and keep multiplying our ratio with parent ratio for the same reason.
+        After the path compression, we will have par[a]=c, ratio[a]=6
+        par[b]=c, ratio[b]=3
+        
+        if to convert from a to b. we will basically do 6//3 = 2. 
+        
+        If no of queries are high, this approach is best suited because
+        we will be able to answer in almost O(1), once the graph is created and compressed. 
+        """
         def find(x):
-            p, xr = root.setdefault(x, (x,1.0))
-            if x!=p:
-                r, pr = find(p)
-                root[x] = (r, xr*pr)
+            xp, xr = root.setdefault(x, (x, 1.0))
+            if x!=xp:
+                tp, tr = find(xp)
+                root[x] = (tp, xr*tr)
             return root[x]
         
         def union(x, y, ratio):
-            px,xr,py,yr = *find(x), *find(y)
+            xp, xr, yp, yr = *find(x), *find(y)
             if not ratio:
-                return xr/yr if px==py else -1.0
-            if px!=py:
-                root[px] = (py, yr/xr*ratio)
+                return xr/yr if xp==yp else -1.0
+            if xp!=yp:
+                root[xp] = (yp, yr/xr*ratio)
                 
-        for (x,y),v in zip(equations, values): 
+        for (x,y),v in zip(equations, values):
             union(x, y, v)
             
-        # print(root)
-        # print(find("a"))
-        # print(find("b"))
-        # print(find("c"))
-        # print(union("a", "b", 0))
-        # print(union("b", "a", 0))
-        
         return [union(x,y,0) if x in root and y in root else -1.0 for x,y in queries]
-    
