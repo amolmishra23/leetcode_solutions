4041fa940147b5bf8a712864527807a54a7f20e8
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-05-19 15:50:51 +0530

    Time: 50 ms (26.05%), Space: 13.8 MB (71.47%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,10 +1,5 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        """
-        Lets solve it using a stack.
-        We have 4 kinds of chars: num, char, [, ]
-        On each type of them, we do different kind of operation
-        """
         stk = [["", 1]]
         num = ""
         
@@ -15,10 +10,9 @@ class Solution:
                 stk.append(["", int(num)])
                 num = ""
             elif x=="]":
-                char, count = stk.pop()
-                stk[-1][0] += char*count
+                ch, count = stk.pop()
+                stk[-1][0] += ch*count
             else:
                 stk[-1][0] += x
-                
+        
         return stk[-1][0]
-                
