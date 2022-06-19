2f0d566313538ab3b9df09499f82ac14edeaa215
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-06-19 10:04:00 +0530

    Time: 263 ms (53.92%), Space: 21.5 MB (26.10%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,29 +1,26 @@
 class TrieNode:
     def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.arr=[]
-
+        self.children = defaultdict(TrieNode)
+        self.arr = []
+        
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-
+        
     def insert(self, word):
         cur = self.root
-        for letter in word:
-            cur = cur.children[letter]
+        for ch in word:
+            cur = cur.children[ch]
             if len(cur.arr)<3: cur.arr.append(word)
-
-    def find(self,word):
-        res=[]
-        cur=self.root
-        for el in word:
-            cur=cur.children[el]
-            res.append(cur.arr)
+                
+    def find(self, word):
+        res, cur = [], self.root
+        for ch in word:
+            cur = cur.children[ch]; res.append(cur.arr)
         return res
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         trie=Trie()
-        for el in sorted(products):
-            trie.insert(el)
+        for prod in sorted(products): trie.insert(prod)
         return trie.find(searchWord)
