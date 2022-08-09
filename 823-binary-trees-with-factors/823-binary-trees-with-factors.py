9b79ade9ee9de4a289e6c86791e7dca03830e2aa
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-08-09 19:13:45 +0530

    Time: 873 ms (29.29%), Space: 15.2 MB (17.68%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,5 +1,14 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        """
+        Logic from https://www.youtube.com/watch?v=BRGz8ArRiPA
+        Solve it using dp.
+        Every num in arr, we need to compare it against all others in array. 
+        To check if num%other_num==0 and num//other_num is in array. So we have got 2 kids for the num.
+        Now number of ways to create num, is equal to product of number of ways we can create its kids. 
+        
+        Answer could be large, hence we keep modding it. 
+        """
         arr, MOD = set(arr), 10**9+7
         
         @lru_cache(None)
