6b3ee67591c4f9accf636bd5d0c942a3b20acdf0
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-09-09 12:28:20 +0530

    Time: 5171 ms (5.04%), Space: 67.8 MB (44.72%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
         properties.sort(key = lambda x: (x[0], -x[1]))
@@ -8,4 +9,16 @@ class Solution:
             stk.append(d)
         
         return len(properties)-len(stk)
+"""
+
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key=lambda x: (x[0], -x[1]))
+        weak, max_defence = 0, 0
+        
+        for a,d in properties[::-1]:
+            if d<max_defence: weak+=1
+            else: max_defence = max(max_defence, d)
+        
+        return weak
         
