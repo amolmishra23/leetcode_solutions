c9d6346661e8026a0be237b7336818c75a8f4e56
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-09-15 04:00:01 +0530

    Time: 1168 ms (75.68%), Space: 86.3 MB (20.36%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -7,7 +7,7 @@
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode], cnt = 0) -> int:
         if not root: return 0
-        cnt ^= 1 << (root.val - 1)
+        cnt ^= 1 << (root.val)
         if root.left is None and root.right is None:
             return 1 if cnt & (cnt - 1) == 0 else 0
         return self.pseudoPalindromicPaths(root.left, cnt) + self.pseudoPalindromicPaths(root.right, cnt)
