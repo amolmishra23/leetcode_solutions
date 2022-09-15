33fab3c9c61642f9f77d36c478768d44e6326ca9
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-09-15 20:46:02 +0530

    Time: 3324 ms (12.75%), Space: 32.2 MB (65.01%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,10 +1,13 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        c = Counter(changed)
-        if c[0]%2: return []
+        count, res = Counter(changed), []
         
-        for x in sorted(c):
-            if c[x]>c[2*x]: return []
-            c[2*x] -= c[x] if x else c[x]//2
-        
-        return list(c.elements())
+        for num in sorted(changed):
+            if count[num]==0: continue
+            if count[2*num]==0: return []
+            res.append(num)
+            if num==0 and count[num]<=1: return []
+            count[num]-=1
+            count[num*2]-=1
+            
+        return res
