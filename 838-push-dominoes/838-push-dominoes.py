b268ba672dc28329067099864cdc3d37b500250e
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-10-01 19:30:04 +0530

    Time: 944 ms (31.96%), Space: 22.2 MB (17.70%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.py
+++ 838-push-dominoes/838-push-dominoes.py
@@ -8,9 +8,14 @@ class Solution:
         while q:
             i, x = q.popleft()
             
+            # if L is still there, and not popped out.
+            # means definitely we need to set at (i-1)th index as L. 
             if x=="L" and i>0 and d[i-1]==".":
                 d[i-1]="L"
                 q.append((i-1, "L"))
+            # in case of R, we need to check if there is an adjacent L, beside dot
+            # if yes, we can ignore this R presence, and also pop adj L
+            # else we just mark the next dot index(i+1) as R
             elif x=="R":
                 if i<len(d)-1 and d[i+1]==".":
                     if i<len(d)-2 and d[i+2]=="L":
@@ -19,6 +24,7 @@ class Solution:
                         d[i+1]="R"
                         q.append((i+1, "R"))
                         
+        # after nothing to be processed in queue, we can return this. 
         return "".join(d)
 
         
