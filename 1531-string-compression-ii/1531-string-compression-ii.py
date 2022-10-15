f0535d751132c220ba6a7cd3d8541122a4ca457d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-10-15 11:28:44 +0530

    Time: 9464 ms (5.52%), Space: 522.2 MB (8.59%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-		# this decorator automatically use memo with key = (start, last, last_count, left)
-        @lru_cache(None)
-        def counter(start, last, last_count, left): #count the cost of compressing from the start
-            if left < 0:
-                return float('inf') # this is impossible
-            if start >= len(s):
-                return 0
-            if s[start] == last:
-				# we have a stretch of the last_count of the same chars, what is the cost of adding one more? 
-                incr = 1 if last_count == 1 or last_count == 9 or last_count == 99 else 0
-				# no need to delete here, if we have a stretch of chars like 'aaaaa' - we delete it from the beginning in the else delete section
-                return incr + counter(start+1, last, last_count+1, left) # we keep this char for compression
-            else:
-				# keep this char for compression - it will increase the result length by 1 plus the cost of compressing the rest of the string 
-                keep_counter = 1 + counter(start+1, s[start], 1, left)
-				# delete this char
-                del_counter =  counter(start + 1, last, last_count, left - 1)
-                return min(keep_counter, del_counter)
-            
-        return counter(0, "", 0, k)
+        return self.dfs(s, 0, "", 0, k)
+    
+    @lru_cache(None)
+    def dfs(self, s, start, prev_char, prev_count, remains):
+        if remains < 0: return float('inf')
+        if start == len(s):
+            return 0
+        if s[start] == prev_char:
+            inc = 1 if prev_count == 1 or prev_count == 9 or prev_count == 99 else 0
+            # keep it 
+            ans1 = inc + self.dfs(s, start+1, prev_char, prev_count + 1, remains)
+            # remove it 
+            ans2 = self.dfs(s, start+1, prev_char, prev_count, remains-1)
+            return min(ans1, ans2)
+        else:
+            # keep it 
+            ans1 = self.dfs(s, start+1, s[start], 1, remains) + 1
+            # remove it 
+            ans2 = self.dfs(s, start+1, prev_char, prev_count, remains-1)
+            return min(ans1, ans2)
