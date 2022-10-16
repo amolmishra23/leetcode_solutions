66d92ee68fc75ec8bc21016c18533906af20830d
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-10-16 18:30:41 +0530

    Time: 5546 ms (18.78%), Space: 524.5 MB (9.70%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -9,11 +9,10 @@ class Solution:
             return 0
         if s[start] == prev_char:
             inc = 1 if prev_count in {1, 9, 99} else 0
-            ans1 = inc + self.dfs(s, start+1, prev_char, prev_count + 1, remains)
-            return ans1
+            return inc + self.dfs(s, start+1, prev_char, prev_count + 1, remains)
         else:
-            # keep it 
+            # keep it (starting a new streak from here)
             ans1 = self.dfs(s, start+1, s[start], 1, remains) + 1
-            # remove it 
+            # remove it (removing the current character. So we have remains-1 attempt left)
             ans2 = self.dfs(s, start+1, prev_char, prev_count, remains-1)
             return min(ans1, ans2)
