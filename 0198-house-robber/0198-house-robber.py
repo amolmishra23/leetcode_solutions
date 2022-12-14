8b211a09caa3d7fe2e194337b759071f99d217a0
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-12-14 11:57:43 +0530

    Time: 39 ms (81.35%), Space: 14 MB (20.34%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        @lru_cache(None)
+        def solve(idx):
+            if idx>=len(nums): return 0
+            
+            return max(solve(idx+2)+nums[idx], solve(idx+1))
+        
+        return solve(0)
