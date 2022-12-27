f4eac62e7d594c675c6f2400b3e51fc1a43e21ff
Amol Mishra <amolsuraj.vit5@gmail.com>
2022-12-27 09:22:21 +0530

    Time: 1050 ms (73.69%), Space: 22.3 MB (32.89%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,18 +1,16 @@
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-        heap, res = [], 0
-        for i, (c,r) in enumerate(zip(capacity, rocks)):
-            if c-r==0:
-                res+=1
-            else:
-                heapq.heappush(heap, [c-r, i])
-            
+        res, heap = 0, []
+        for c, r in zip(capacity, rocks):
+            if c-r==0: res += 1
+            else: heapq.heappush(heap, c-r)
+                
         while additionalRocks and heap:
-            rem, idx = heapq.heappop(heap)
+            rem = heapq.heappop(heap)
             if rem<=additionalRocks:
                 res += 1
                 additionalRocks -= rem
             else:
                 return res
-            
+        
         return res
