98c744427a3f717d651289885d05dbb3fdbf7975
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-03-22 08:00:55 +0530

    Time: 1588 ms (96.98%), Space: 75.9 MB (34.56%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,11 +1,20 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
+        """
+        return min([z for _,_,z in roads])
+        above code passes 38/41 cases. Where it fails is, when we find min edge in another disconnected component.
+        So the goal is start from 1, and howsoever the graph extends, find minimum edge in this entire component.,
+        going long route is totally okay. find the min score. 
+        """
         graph = defaultdict(dict)
         for x,y,z in roads: graph[x][y]=graph[y][x]=z
         
         q, visited, res = deque(), set(), float("inf")
         q.append(1)
         
+        # No need to check if we reached final dest or not. 
+        # Because problem spec has specified at least one path there
+        # Even if we reached final dest, still travel all routes in hope of finding a minimum score, and only then return
         while q:
             curr = q.popleft()
             
