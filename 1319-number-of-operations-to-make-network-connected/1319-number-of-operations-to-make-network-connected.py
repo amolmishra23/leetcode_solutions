c10a03e687622d90fad11097790d2ad36c72ddbf
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-03-23 07:38:18 +0530

    Time: 516 ms (59.40%), Space: 34 MB (68.05%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        par, count, components = {}, defaultdict(lambda: 1), n
+        
+        def find(x):
+            par.setdefault(x, x)
+            if par[x]==x: return par[x]
+            par[x] = find(par[x])
+            return par[x]
+        
+        def union(x, y):
+            nonlocal components
+            px, py = find(x), find(y)
+            if px==py: return False
+            if count[py]>count[px]:
+                par[px]=py; count[py]+=count[px]
+            else:
+                par[py]=px; count[px]+=count[py]
+            components -= 1
+            return True
+        
+        extra_connections = 0
+        for x,y in connections: 
+            if not union(x,y): extra_connections+=1
+        
+        return components-1 if extra_connections>=components-1 else -1
