22cc03bd9805d4e5699848a709c4bd78cbd18d84
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-04-03 07:25:11 +0530

    Time: 465 ms (57.49%), Space: 21 MB (18.19%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,25 +1,40 @@
+# class Solution:
+#     def numRescueBoats(self, people: List[int], limit: int) -> int:
+#         people.sort()
+#         def possible(boats):
+#             l, h = 0, len(people)-1
+            
+#             while l<=h:
+#                 if people[l]+people[h]<=limit:
+#                     l+=1; h-=1; boats-=1
+#                 else:
+#                     h-=1; boats-=1
+#                 if boats<0: return False
+                
+#             return boats>=0
+            
+#         l, r = 1, len(people)
+        
+#         while l<r:
+#             m = l + (r-l)//2
+#             if possible(m):
+#                 r = m
+#             else:
+#                 l = m+1
+        
+#         return l
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort()
-        def possible(boats):
-            l, h = 0, len(people)-1
-            
-            while l<=h:
-                if people[l]+people[h]<=limit:
-                    l+=1; h-=1; boats-=1
-                else:
-                    h-=1; boats-=1
-                if boats<0: return False
-                
-            return boats>=0
-            
-        l, r = 1, len(people)
-        
-        while l<r:
-            m = l + (r-l)//2
-            if possible(m):
-                r = m
+        people = sorted(people)
+        lo = 0
+        hi = len(people)-1
+        boats = 0
+        while lo <= hi:
+            if people[lo] + people[hi] <= limit:
+                lo += 1
+                hi -= 1
             else:
-                l = m+1
-        
-        return l
+                hi -= 1
+            boats += 1
+        return boats
