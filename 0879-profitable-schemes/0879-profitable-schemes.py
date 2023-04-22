794c4765c869dbd10e58e8457109b5b5db1f0334
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-04-22 17:15:24 +0530

    Time: 4194 ms (18.27%), Space: 632.8 MB (16.35%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def profitableSchemes(self, n: int, min_profit: int, group: List[int], profit: List[int]) -> int:
+        MOD = 10**9+7
+        
+        @lru_cache(None)
+        def dp(i, curr_profit, curr_people):
+            if i>=len(group): return curr_profit>=min_profit
+            
+            res = 0
+            if curr_people+group[i] <= n:
+                res += (dp(i+1, min(curr_profit+profit[i], min_profit), curr_people+group[i]))%MOD
+            res += (dp(i+1, curr_profit, curr_people))%MOD
+            
+            return res
+        
+        return dp(0, 0, 0)%MOD
