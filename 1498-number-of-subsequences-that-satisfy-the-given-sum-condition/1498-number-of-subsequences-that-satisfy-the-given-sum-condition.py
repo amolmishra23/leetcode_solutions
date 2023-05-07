ebe5e9e5f0648661ecb9b759a5764df1b7dd2ce8
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-05-07 12:22:38 +0530

    Time: 834 ms (68.09%), Space: 28.4 MB (19.10%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -4,10 +4,17 @@ class Solution:
         l, r = 0, len(A) - 1
         res = 0
         mod = 10**9 + 7
+        
         while l <= r:
             if A[l] + A[r] > target:
+                # everytime we arent less than target, we reduce right pointer
                 r -= 1
             else:
+                # time to record an answer. Because keeping index l constant, 
+                # in the numbers from (r-l), we can have 2**(r-l) subsequences. 
+                # meanwhile also keep modding the answer
                 res += pow(2, r - l, mod)
                 l += 1
+        
         return res % mod
+    
