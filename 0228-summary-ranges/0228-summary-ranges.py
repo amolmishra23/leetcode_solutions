bac38a55b9ed06e4715d2b7d51f56721e657e9ad
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-06-12 20:52:39 +0530

    Time: 42 ms (66.23%), Space: 16.4 MB (26.14%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,12 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ranges = []
+        
+        for n in nums:
+            if ranges and ranges[-1][-1]+1==n:
+                ranges[-1][-1]=n
+            else:
+                ranges.append([n, n])
+        
+        return [str(x)+"->"+str(y) if x!=y else str(x) for x,y in ranges]
+        
