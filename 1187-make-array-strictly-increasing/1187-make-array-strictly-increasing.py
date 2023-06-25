e28519aeabd74cef6602bbcb92799af810cf9417
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-06-25 10:43:50 +0530

    Time: 987 ms (34.88%), Space: 153.2 MB (14.47%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
@@ -11,18 +11,26 @@ class Solution:
         
         @functools.lru_cache(None)
         def attempt(index, must_strictly_bigger_than):
+            # If we reached the end, it is a success
             if index == len(arr1):
                 return 0
+            
+            # Its already big enough. Dont need to replace
             if arr1[index] > must_strictly_bigger_than:
                 res1 = attempt(index + 1, arr1[index])
             else:
                 res1 = float('inf')
+                
+            # Lets replace it simply, with bigger element. (Trying all permutations)
             bigger = getSlightlyStrightlyBigger(must_strictly_bigger_than)
             if bigger is None:
                 res2 = float('inf')
             else:
                 res2 = attempt(index + 1, bigger)
+            
+            # returning whichever workflow returned us minimum steps overall. 
             return min(res1, res2 + 1)
                 
+        # initially we just need to be bigger than float("-inf")
         res = attempt(0, float('-inf'))
         return res if res != float('inf') else -1
