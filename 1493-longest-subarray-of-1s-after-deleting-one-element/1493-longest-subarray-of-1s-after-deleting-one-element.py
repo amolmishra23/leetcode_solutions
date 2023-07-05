7a30328122333adcc283796ae0f99e9fde56ad92
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-07-05 18:53:11 +0530

    Time: 406 ms (34.16%), Space: 19.1 MB (11.96%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,12 +1,32 @@
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         res = prev = curr = 0
+        
+#         for bit in nums:
+#             if bit:
+#                 curr += 1
+#                 res = max(res, curr+prev)
+#             else:
+#                 prev, curr = curr, 0
+                
+#         return res if res!=len(nums) else res-1
+
+
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        res = prev = curr = 0
+        i, res, count = 0, 0, 0
         
-        for bit in nums:
-            if bit:
-                curr += 1
-                res = max(res, curr+prev)
-            else:
-                prev, curr = curr, 0
+        for j in range(len(nums)):
+            if nums[j]==0:
+                count += 1
                 
+            while count>1:
+                if nums[i]==0:
+                    count-=1
+                i += 1
+            
+            res = max(res, j-i+1-count)
+            
+        # because 1 number needs to be deleted mandatorily
         return res if res!=len(nums) else res-1
+            
