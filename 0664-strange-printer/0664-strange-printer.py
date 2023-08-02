bc5e7bcc994a79c409d50c4bb14c6264de66403e
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-08-02 21:37:58 +0530

    Time: 1237 ms (35.35%), Space: 21.7 MB (7.58%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -2,12 +2,16 @@ class Solution:
     def strangePrinter(self, s: str) -> int:
         @lru_cache(None)
         def solve(i, j):
+            # for 1 character string, we need 1 attempt for sure. 
             if i==j: return 1
             
             res = float("inf")
+            # same like matrix chain multiplication we break at every point
             for k in range(i, j):
                 res = min(res, solve(i, k) + solve(k+1, j))
                 
+            # subtract 1 because if 1st and last char are same. 1 try less needed.
+            # to understand better try the examples: aa, aaba, aabab etc
             return res-1 if s[i]==s[j] else res
         
         return solve(0, len(s)-1)
