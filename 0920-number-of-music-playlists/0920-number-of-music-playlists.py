32e81c0734368934690b554e25bc05f7fce86607
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-08-06 18:29:16 +0530

    Time: 99 ms (31.58%), Space: 20.4 MB (6.02%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,10 +1,13 @@
 class Solution:
-    def numMusicPlaylists(self, N, L, K):
-        dp = [[0 for i in range(L + 1)] for j in range(N + 1)]
-        for i in range(K + 1, N + 1):
-            for j in range(i, L + 1):
-                if i == j or i == K + 1:
-                    dp[i][j] = math.factorial(i)
-                else:
-                    dp[i][j] = dp[i - 1][j - 1] * i + dp[i][j - 1] * (i - K)
-        return dp[N][L] % (10**9 + 7)
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        MOD = (10**9)+7
+        @lru_cache(None)
+        def solve(currIdx, usedCount):
+            if currIdx == goal: return usedCount==n
+            
+            reuseCount = (max(0, usedCount-k) * solve(currIdx+1, usedCount))%MOD
+            newCount = ((n-usedCount) * solve(currIdx+1, usedCount+1))%MOD
+            
+            return (reuseCount+newCount) % MOD
+        
+        return solve(0, 0)
