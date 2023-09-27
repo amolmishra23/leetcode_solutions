44006647a949e59a3cd9a16ecb581091454e4e97
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-09-27 21:01:46 +0530

    Time: 33 ms (86.36%), Space: 16.1 MB (98.48%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def decodeAtIndex(self, S: str, K: int) -> str:
+        # soln: https://www.youtube.com/watch?v=p-GPuy01a7A
+        lens, n = [0], len(S)
+        for c in S:
+            if c.isdigit():
+                lens.append(lens[-1]*int(c))
+            else:
+                lens.append(lens[-1]+1)
+                
+        for i in range(n, 0, -1):
+            K %= lens[i]
+            if K==0 and S[i-1].isalpha(): return S[i-1]
+            
+        
