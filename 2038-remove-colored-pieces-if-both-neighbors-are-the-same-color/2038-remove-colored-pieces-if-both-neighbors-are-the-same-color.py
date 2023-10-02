e7e71207e476687d82e320727c477db0c869cdf8
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-10-02 08:51:54 +0530

    Time: 191 ms (72.51%), Space: 17.3 MB (65.73%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,5 +1,20 @@
 class Solution:
-    def winnerOfGame(self, colors: str) -> bool:
+    def winnerOfGame(self, a: str) -> bool:
+        a_count, b_count = 0,0
+        for i in range(1, len(a)-1):
+            if a[i-1]==a[i]==a[i+1]:
+                if a[i]=="A":
+                    a_count += 1
+                else:
+                    b_count += 1
+        
+        return a_count > b_count
+        
+    def winnerOfGame2(self, colors: str) -> bool:
+        """
+        logic is to find extra a,b (in the continous streak of 3)
+        And figure which has most extra in the end
+        """
         a, curr_a = 0,0
         b, curr_b = 0, 0
         
