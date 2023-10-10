243faffaf9b02adb352a2c42729bd6aa98db544a
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-10-10 20:45:15 +0530

    Time: 691 ms (75.00%), Space: 35.4 MB (51.32%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -1,4 +1,16 @@
 class Solution:
+    def minOperations1(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = sorted(set(nums))  
+
+        ans = n
+        for i, start in enumerate(nums):
+            end = start + n - 1 
+            idx = bisect_right(nums, end)
+            uniqueLen = idx - i
+            ans = min(ans, n - uniqueLen)
+        return ans
+    
     def minOperations(self, nums: List[int]) -> int:
         length = len(nums)
         nums = sorted(set(nums))
@@ -6,8 +18,8 @@ class Solution:
         res = float("inf")
         
         for l in range(len(nums)):
-            # expecting numbers to be in range nums[l]+length
-            while r<len(nums) and nums[r]<nums[l]+length:
+            # expecting numbers to be in range nums[l]+length-1
+            while r<len(nums) and nums[r]<=nums[l]+length-1:
                 r+=1
             res = min(res, length-(r-l))
             
