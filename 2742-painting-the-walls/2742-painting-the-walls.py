9d90bcc795a457dc383795f067526b86a42fdd25
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-10-14 12:22:33 +0530

    Time: 2144 ms (32.96%), Space: 444.8 MB (28.15%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -11,6 +11,11 @@ class Solution:
             
             # lets paint the current wall.
             # lets skip paiting the current wall.
+            
+            # we have only condensed it to 2 variable dp.
+            # no need to track time. Because instead of tracking time,
+            # we keep reducing that many walls in the remaining walls.
+            # ultimately if we have 0 remaining walls, that anyways is our soln. 
             return min(cost[i] + dp(i+1, rem - 1 - time[i]), dp(i+1, rem))
         
         return dp(0, len(time))
