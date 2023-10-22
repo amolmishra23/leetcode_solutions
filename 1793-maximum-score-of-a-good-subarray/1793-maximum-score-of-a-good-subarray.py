6d10bb2b2348fe9bbcc1128f3ab17d5177e12501
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-10-22 09:22:12 +0530

    Time: 997 ms (80.10%), Space: 26.7 MB (93.88%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -1,5 +1,11 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
+        """
+        We could choose greedy here, because it is always going to be continuous subarray
+        So if we have a min element, it will stay minimum(even in subsequent turns). 
+        Not dp like thing. 
+        So greedy will just work fine here
+        """
         i, j = k-1, k+1
         res = nums[k]
         curr_min, curr_len = res, 1
@@ -18,3 +24,4 @@ class Solution:
             res = max(res, curr_min * curr_len)
                 
         return res
+    
