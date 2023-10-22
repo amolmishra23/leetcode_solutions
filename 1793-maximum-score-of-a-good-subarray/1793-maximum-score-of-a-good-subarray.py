355b6f185e9d5031c66aed6e93155f293aa9f3af
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-10-22 09:14:04 +0530

    Time: 1017 ms (73.98%), Space: 26.8 MB (77.04%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        i, j = k-1, k+1
+        res = nums[k]
+        curr_min, curr_len = res, 1
+        
+        while i>=0 or j<len(nums):
+            opt1 = nums[i] if i>=0 else float("-inf")
+            opt2 = nums[j] if j<len(nums) else float("-inf")
+            
+            if opt1>opt2:
+                curr_min = min(curr_min, nums[i])
+                curr_len += 1; i-=1
+            else:
+                curr_min = min(curr_min, nums[j])
+                curr_len += 1; j+=1
+                
+            res = max(res, curr_min * curr_len)
+                
+        return res
