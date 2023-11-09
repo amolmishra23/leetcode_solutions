7597c4edaf77ee243fcecc76b2d595f9b5eed5a8
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-11-09 20:26:58 +0530

    Time: 157 ms (23.02%), Space: 17.3 MB (52.88%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -1,5 +1,10 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
+        """
+        Logic here is that, we keep moving until we find a homogenous string
+        from that totally we can get n*(n+1)/2 substring
+        we keep adding it to our res, and keep modding it
+        """
         MOD = (10**9)+7
         i, res = 0, 0
         
