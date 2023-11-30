17c8b03652ac28eea901c727bc3aa40ceece20c8
Amol Mishra <amolsuraj.vit5@gmail.com>
2023-11-30 21:50:30 +0530

    Time: 47 ms (24.03%), Space: 16.3 MB (27.27%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.py
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.py
@@ -1,7 +1,9 @@
 class Solution:
+    dp = {0: 0}
     def minimumOneBitOperations(self, n):
-        res = 0
-        while n:
-            res = -res - (n ^ (n - 1))
-            n &= n - 1
-        return abs(res)
+        if n not in self.dp:
+            b = 1
+            while (b << 1) <= n:
+                b = b << 1
+            self.dp[n] = self.minimumOneBitOperations((b >> 1) ^ b ^ n) + 1 + b - 1
+        return self.dp[n]
