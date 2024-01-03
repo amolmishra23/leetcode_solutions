1b4885d1304f8e61eb772c3d7bb3f97869a720ae
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-01-03 09:04:08 +0530

    Time: 83 ms (95.55%), Space: 19.5 MB (12.99%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,7 +1,5 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        onesCount = list(map(lambda x: x.count("1"), bank))
-        onesCount = [num for num in onesCount if num>0]
+        onesCount = [num for num in list(map(lambda x: x.count("1"), bank)) if num>0]
         return sum(a*b for a, b in zip(onesCount[:-1], onesCount[1:]))
         
-            
