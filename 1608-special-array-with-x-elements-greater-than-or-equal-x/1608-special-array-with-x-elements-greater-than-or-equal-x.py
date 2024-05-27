0284264bb7a92f931ab8c2893411dde1fa89f0eb
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-05-27 21:37:38 +0530

    Time: 37 ms (79.23%), Space: 16.5 MB (48.53%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,5 +1,21 @@
 class Solution:
-    def specialArray(self, nums: List[int]) -> int:
+    def specialArray(self, arr: List[int]) -> int:
+        arr.sort()
+        def count(arr, val):
+            idx = bisect.bisect_left(arr, val)
+            return len(arr)-idx
+        
+        lo, hi = 0, len(arr)
+        while lo<=hi:
+            mi = lo + (hi-lo)//2
+            g_count = count(arr, mi)
+            if g_count == mi: return mi
+            elif g_count > mi: lo = mi+1
+            else: hi = mi - 1
+                
+        return -1 
+    
+    def specialArray1(self, nums: List[int]) -> int:
         count = [0]*(len(nums)+1)
         
         for n in nums:
