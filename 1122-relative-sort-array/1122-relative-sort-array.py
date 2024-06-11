ab1e5f8b13d6de33826665048f63f12f4c08383c
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-06-11 16:42:34 +0530

    Time: 46 ms (27.87%), Space: 16.7 MB (67.06%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,6 +1,9 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        return sorted(arr1, key=(arr2 + sorted(arr1)).index)
+    
+    def relativeSortArray1(self, arr1: List[int], arr2: List[int]) -> List[int]:
         arr1_set, arr2_set, count = set(arr1), set(arr2), Counter(arr1)
         diff, res = sorted(arr1_set.difference(arr2_set)), []
         for x in arr2+diff: res.extend([x]*count[x])
-        return res        
+        return res
