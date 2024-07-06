eb03c4726b68f79d0a16592ac096eb2e5f2151eb
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-07-06 18:33:59 +0530

    Time: 40 ms (23.04%), Space: 16.7 MB (7.09%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,6 +1,16 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
         """
+        Each start to end to go, we need n-1 moves
+        if rounds_done is even => we now will start from left to right
+        if rounds_done is odd => we will start from right to left
+        accordingly calculate and return the result. 
+        """
+        rounds_done, rem_moves = divmod(time,n-1)
+        return n-rem_moves if rounds_done%2 else rem_moves+1
+    
+    def passThePillow1(self, n: int, time: int) -> int:
+        """
         if we have n people, we need n-1 to completely traverse once. 
         """
         if time<n:
