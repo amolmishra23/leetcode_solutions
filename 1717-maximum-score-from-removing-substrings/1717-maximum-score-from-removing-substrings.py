113f53f46b94d5daa12a4ccbeb0496322a9a8501
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-07-12 18:52:32 +0530

    Time: 231 ms (73.38%), Space: 18.7 MB (15.58%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,23 +1,16 @@
 class Solution:
-    def maximumGain(self, s: str, x: int, y: int) -> int:
-        def solve(s, ch1, ch2):
+    def maximumGain(self, s: str, x: int, y: int) -> int:            
+        def solve(pair, score):
+            nonlocal s
             res, stk = 0, []
             for ch in s:
-                if ch==ch2 and stk and stk[-1]==ch1: 
-                    stk.pop(); res+=x
+                if ch==pair[1] and stk and stk[-1]==pair[0]: 
+                    stk.pop(); res+=score
                 else:
                     stk.append(ch)
             
             s = "".join(stk)
-            stk = []
-            for ch in s:
-                if ch==ch1 and stk and stk[-1]==ch2:
-                    stk.pop(); res+=y
-                else:
-                    stk.append(ch)
-            
             return res
         
         mx = "ab" if x>y else "ba"
-        if y>x: x,y=y,x
-        return solve(s, mx[0], mx[1])
+        return solve(mx, max(x, y)) + solve(mx[::-1], min(x,y))
