8b9666f6bfb0568fc6170d2a3855ed1201359091
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-07-30 22:59:34 +0530

    Time: 763 ms (12.67%), Space: 25.3 MB (6.00%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -4,17 +4,15 @@ class Solution:
         prefix_a = [0] * (n + 1)
         prefix_b = [0] * (n + 1)
         
-        # Fill prefix arrays
         for i in range(n):
             prefix_a[i + 1] = prefix_a[i] + (s[i] == 'a')
             prefix_b[i + 1] = prefix_b[i] + (s[i] == 'b')
         
         res = float('inf')
         
-        # Compute minimum deletions
         for i in range(n + 1):
-            b_before = prefix_b[i]    # b's before position i
-            a_after = prefix_a[n] - prefix_a[i]  # a's after position i
+            b_before = prefix_b[i]
+            a_after = prefix_a[n] - prefix_a[i]
             res = min(res, b_before + a_after)
         
         return res
