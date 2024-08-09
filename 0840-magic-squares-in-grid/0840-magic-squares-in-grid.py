e45e46056a294a410cfe34d9a356c2edd1ed3ae5
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-08-09 23:29:27 +0530

    Time: 15 ms (87.50%), Space: 11.7 MB (31.25%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,18 +1,37 @@
 class Solution(object):
-    def numMagicSquaresInside(self, g):
+    def numMagicSquaresInside(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        def is_magic(i, j):
-            s = "".join(
-                str(g[i+x//3][j+x%3]) for x in [0,1,2,5,8,7,6,3]
-            )
-            return g[i][j]%2==0 and (
-                s in "43816729"*2 or s in "43816729"[::-1]*2
-            )
+        def magic(r,c):
+            values = set()
+            
+            for i in range(r, r+3):
+                for j in range(c, c+3):
+                    if grid[i][j] in values or not 1<=grid[i][j]<=9:
+                        return 0
+                    values.add(grid[i][j])
+                    
+            for i in range(r, r+3):
+                curr_sum = 0
+                for j in range(c, c+3):
+                    curr_sum += grid[i][j]
+                if curr_sum!=15: return 0
+            
+            for j in range(c, c+3):
+                curr_sum = 0
+                for i in range(r, r+3):
+                    curr_sum += grid[i][j]
+                if curr_sum!=15: return 0
+                
+            diag1 = grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2]
+            diag2 = grid[r+2][c]+grid[r+1][c+1]+grid[r][c+2]
+            return diag1==diag2==15
         
-        m, n = len(g), len(g[0])
-        return sum(
-            is_magic(i, j) for i in range(m-2) for j in range(n-2) if g[i+1][j+1]==5
-        )
+        res = 0
+        for r in range(len(grid)-2):
+            for c in range(len(grid[0])-2):
+                res += magic(r, c)
+                
+        return res
