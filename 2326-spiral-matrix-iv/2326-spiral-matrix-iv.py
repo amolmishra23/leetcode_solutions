0700af71249be8c91f263105f3af8ee2b98a4832
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-09-09 14:39:52 +0530

    Time: 699 ms (39.28%), Space: 56.5 MB (34.49%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -5,6 +5,24 @@
 #         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        r, c = 0, 0
+        xr, xc = 0, 1
+        
+        matrix = [[-1 for _ in range(n)] for _ in range(m)]
+        
+        while head:
+            matrix[r][c] = head.val
+            
+            if r+xr <0 or r+xr>=m or c+xc<0 or c+xc>=n or matrix[r+xr][c+xc]!=-1:
+                xr, xc = xc, -xr
+                
+            r += xr
+            c += xc
+            head = head.next
+            
+        return matrix
+        
+    def spiralMatrix1(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         res = [[-1]*n for _ in range(m)]
         
         def traverse(root):
