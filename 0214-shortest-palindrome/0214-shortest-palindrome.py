dadd027a0b0b79921ad1bde0f3516b645e584e52
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-09-20 09:28:38 +0530

    Time: 74 ms (46.87%), Space: 20.7 MB (8.96%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        def compute_lps(pat):
+            n = len(pat)
+            res = [0]*n
+            
+            for i in range(1, n):
+                j = res[i-1]
+                
+                while j>0 and pat[i] != pat[j]:
+                    j = res[j-1]
+                    
+                if pat[i]==pat[j]: j+=1
+                    
+                res[i] = j
+                
+            return res
+        
+        lps = compute_lps(s + "#" + s[::-1])
+        
+        return s[lps[-1]:][::-1] + s
