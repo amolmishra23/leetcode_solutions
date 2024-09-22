9843db9a0efbdadb0fc07c036c5a355b86fe7af4
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-09-22 19:57:48 +0530

    Time: 25 ms (99.00%), Space: 16.5 MB (30.85%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -1,5 +1,10 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
+        """
+        Best explanation at: https://www.youtube.com/watch?v=wRubz1zhVqk
+        Logic is, at each dfs branch we keep counting how many elements are down there.
+        if we have the element, we keep adding to counter. 
+        """
         curr, i = 1, 1
         
         def count(curr):
