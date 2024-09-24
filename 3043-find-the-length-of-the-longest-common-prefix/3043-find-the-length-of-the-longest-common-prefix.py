2803cb64da6bb44d34abb841b924416c9408a6e6
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-09-24 19:04:23 +0530

    Time: 962 ms (44.90%), Space: 32.8 MB (28.65%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,4 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.val = {}
+        self.is_end = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, a):
+        curr = self.root
+        tmp = str(a)
+        
+        for ch in tmp:
+            if ch not in curr.val:
+                curr.val[ch] = TrieNode()
+            curr = curr.val[ch]
+            
+        curr.is_end = True
+        
+    def prefix_match(self, b):
+        curr = self.root
+        tmp = str(b)
+        res = 0
+        
+        for ch in tmp:
+            if ch not in curr.val:
+                break
+            curr = curr.val[ch]
+            res += 1
+            
+        return res
+        
 class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        t = Trie()
+        
+        for val in arr1:
+            t.insert(val)
+            
+        res = 0
+        for val in arr2:
+            res = max(res, t.prefix_match(val))
+            
+        return res
+
+class Solution1:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         if len(arr1) > len(arr2):
             arr1, arr2 = arr2, arr1
