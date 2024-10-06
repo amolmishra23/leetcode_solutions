ef4bbd4111cb25810300898dd3e4ccba7e2d0014
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-10-06 08:36:47 +0530

    Time: 34 ms (62.11%), Space: 16.7 MB (13.04%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -1,5 +1,16 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        dq1, dq2 = deque(sentence1.split()), deque(sentence2.split())
+        
+        while dq1 and dq2 and dq1[0]==dq2[0]:
+            dq1.popleft(); dq2.popleft()
+            
+        while dq1 and dq2 and dq1[-1]==dq2[-1]:
+            dq1.pop(); dq2.pop()
+            
+        return not dq1 or not dq2
+        
+    def areSentencesSimilar1(self, sentence1: str, sentence2: str) -> bool:
         s1, s2 = sentence1.split(" "), sentence2.split(" ")
         
         n1, n2 = len(s1), len(s2)
