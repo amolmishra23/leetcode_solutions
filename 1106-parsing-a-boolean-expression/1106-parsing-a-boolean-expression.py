9e82ef179637f33986f372c5f68a2efa59266294
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-10-20 14:09:05 +0530

    Time: 12 ms (100.00%), Space: 16.6 MB (95.21%) - LeetHub

---
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -0,0 +1,29 @@
+class Solution:
+    def parseBoolExpr(self, s: str) -> bool:
+        stk = []
+        
+        for c in s:
+            if c!=")" and c!=",":
+                stk.append(c)
+            elif c == ")":
+                exp = []
+                
+                while stk and stk[-1] != "(":
+                    exp.append(stk.pop() == "t")
+                    
+                stk.pop()
+                
+                if stk:
+                    t = stk.pop()
+                    res = None
+                    
+                    if t == "&":
+                        res = all(exp)
+                    elif t == "|":
+                        res = any(exp)
+                    else:
+                        res = not exp[0]
+                        
+                    stk.append("t" if res else "f")
+                    
+        return stk[-1] == "t"
