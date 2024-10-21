f286ea48e8ca8560eed0ed6181cf0fa495c99164
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-10-21 07:07:05 +0530

    Time: 196 ms (40.58%), Space: 16.6 MB (19.96%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -3,14 +3,14 @@ class Solution:
         seen = set()
         def solve(i, seen):
             if i==len(s):
-                return len(seen)
+                return 0
             
             res = 0
             
             for j in range(i, len(s)):
                 if s[i:j+1] not in seen:
                     seen.add(s[i:j+1])
-                    res = max(res, solve(j+1, seen))
+                    res = max(res, 1+solve(j+1, seen))
                     seen.remove(s[i:j+1])
                     
             return res
