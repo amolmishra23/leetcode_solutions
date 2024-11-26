147d7242414268c1184fea60f01dd8e27d066b6d
Amol Mishra <amolsuraj.vit5@gmail.com>
2024-11-26 19:06:33 +0530

    Time: 8 ms (55.03%), Space: 17.6 MB (5.07%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.py
+++ 0773-sliding-puzzle/0773-sliding-puzzle.py
@@ -1,46 +1,33 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
-        # Target state and initial state
-        target = "123450"
-        start = ''.join(str(num) for row in board for num in row)
-        
-        # Neighbors map for each index in the 1D representation of the board
-        neighbors = {
-            0: [1, 3], 1: [0, 2, 4], 2: [1, 5],
-            3: [0, 4], 4: [1, 3, 5], 5: [2, 4]
+        neigh = {
+            0: [1,3],
+            1: [0, 4, 2],
+            2: [1, 5],
+            3: [0, 4],
+            4: [1,3,5],
+            5: [2,4],
         }
-        
-        # BFS setup
-        queue = deque([(start, 0)])  # (state, moves)
-        visited = set()
-        visited.add(start)
+        start = "".join([str(elem) for row in board for elem in row])
+        queue = deque(); queue.append((start, 0))
+        visited = set(); visited.add(start)
         
         while queue:
             state, moves = queue.popleft()
             
-            # Check if we've reached the target
-            if state == target:
+            if state == "123450":
                 return moves
             
-            # Find the index of zero
-            zero_index = state.index('0')
+            zero_index = state.index("0")
             
-            # Generate new states by swapping '0' with its neighbors
-            for neighbor in neighbors[zero_index]:
-                # Convert state to a list for mutation
+            for n in neigh[zero_index]:
                 new_state = list(state)
-                # Swap '0' with the neighbor
-                new_state[zero_index], new_state[neighbor] = new_state[neighbor], new_state[zero_index]
-                # Convert back to string
-                new_state_str = ''.join(new_state)
+                new_state[zero_index], new_state[n] = new_state[n], new_state[zero_index]
+                new_state_str = "".join(new_state)
                 
-                # If this new state hasn't been visited, add it to the queue
                 if new_state_str not in visited:
                     visited.add(new_state_str)
-                    queue.append((new_state_str, moves + 1))
-        
-        # If we exhaust the queue without finding the target
+                    queue.append((new_state_str, moves+1))
+                    
         return -1
+        
