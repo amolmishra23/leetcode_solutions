30a95d6a9a4225fe6d0f23d6b13e14f32a9d1921
Amol Mishra <amolsuraj.vit5@gmail.com>
2025-05-03 17:13:29 +0530

    Time: 246 ms (43.24%), Space: 28 MB (7.92%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.py
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -1,30 +1,28 @@
 class Solution:
-    def pushDominoes(self, d: str) -> str:
-        d, q = list(d), deque()
+    def pushDominoes(self, dominoes: str) -> str:
+        d= list(dominoes)
+
+        q = deque([(x,i) for i, x in enumerate(d) if x!="."])
+        print(q)
         
-        for i,x in enumerate(d): 
-            if x!=".": q.append((i, x))
-            
         while q:
-            i, x = q.popleft()
-            
-            # if L is still there, and not popped out.
-            # means definitely we need to set at (i-1)th index as L. 
-            if x=="L" and i>0 and d[i-1]==".":
+            curr, i = q.popleft()
+
+            if curr == "L" and i>0 and d[i-1]==".":
+                # making the next domino tilt
                 d[i-1]="L"
-                q.append((i-1, "L"))
-            # in case of R, we need to check if there is an adjacent L, beside dot
-            # if yes, we can ignore this R presence, and also pop adj L
-            # else we just mark the next dot index(i+1) as R
-            elif x=="R":
+                # this step is important, because we keep making the domino fall
+                q.append(("L", i-1))
+            elif curr == "R":
                 if i<len(d)-1 and d[i+1]==".":
                     if i<len(d)-2 and d[i+2]=="L":
+                        # because the right and left are balancing each other, we remove the left tilt
                         q.popleft()
                     else:
-                        d[i+1]="R"
-                        q.append((i+1, "R"))
-                        
-        # after nothing to be processed in queue, we can return this. 
-        return "".join(d)
+                        # making the next domino tilt
+                        d[i+1] = "R"
+                        # this step is important, because we keep making the domino fall
+                        q.append(("R", i+1))
 
-        
+        return "".join(d)
+            
