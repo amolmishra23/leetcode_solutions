8ab01e018b6e364be854a945f408ebf7b66ba53b
Amol Mishra <amolsuraj.vit5@gmail.com>
2025-05-26 21:40:47 +0530

    Time: 1974 ms (53.94%), Space: 90.5 MB (53.71%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
@@ -1,5 +1,33 @@
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        graph, indegree, n = defaultdict(list), defaultdict(int), len(colors)
+        for u,v in edges: graph[u].append(v); indegree[v]+=1
+        count = [[0]*26 for _ in range(n)]
+
+        queue, visited_count, res = deque(), 0, 0
+        for x in range(len(colors)):
+            if indegree[x]==0: queue.append(x)
+
+        while queue:
+            for _ in range(len(queue)):
+                visited_count += 1
+                curr_node = queue.popleft()
+                curr_color = ord(colors[curr_node])-ord("a")
+                count[curr_node][curr_color] += 1
+                res = max(res, count[curr_node][curr_color])
+
+                for neigh in graph[curr_node]:
+                    for c in range(26):
+                        count[neigh][c] = max(
+                            count[neigh][c],
+                            count[curr_node][c],
+                        )
+                    indegree[neigh]-=1
+                    if indegree[neigh]==0: queue.append(neigh)
+
+        return res if visited_count==len(colors) else -1
+
+    def largestPathValue1(self, colors: str, edges: List[List[int]]) -> int:
         graph, n = defaultdict(list), len(colors)
         for u,v in edges: graph[u].append(v)
         count = [[0]*26 for _ in range(n)]
