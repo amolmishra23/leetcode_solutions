de3d653767b9388c3491992ea4b64f6fb0cf71eb
Amol Mishra <amolsuraj.vit5@gmail.com>
2025-06-09 20:45:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (64.68%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -1,30 +1,36 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        """
-        Best explanation at: https://www.youtube.com/watch?v=wRubz1zhVqk
-        Logic is, at each dfs branch we keep counting how many elements are down there.
-        if we have the element, we keep adding to counter. 
-        """
-        curr, i = 1, 1
-        
+        # curr is the branch
+        # i is the number of elements traversed
+        # https://www.youtube.com/watch?v=wRubz1zhVqk
+        curr, i = 1,1
+
         def count(curr):
+            # Assume n=100
+            # curr=1, nei=2, we get 1 element here
+            # curr=10, nei=20, we get 10 elements here
+            # curr=100, nei=200, we get 100 elements here
             res, nei = 0, curr+1
-            
-            while curr <= n:
-                res += min(nei, n+1) - curr
-                curr *= 10
-                nei *= 10
-                
+
+            while curr<=n:
+                res += min(nei, n+1)-curr
+                curr*=10
+                nei*=10
+
             return res
-        
-            
+
         while i<k:
             steps = count(curr)
-            if i + steps <= k:
-                curr = curr + 1
-                i += steps 
+            if i+steps <= k:
+                # increment branch by 1
+                curr = curr+1
+                # increment i by number of steps 
+                i+=steps
             else:
+                # diving deeper in the branch
                 curr *= 10
-                i += 1
-            
+                # as we passed by current number, we add 1. 
+                i+=1
+
         return curr
+
