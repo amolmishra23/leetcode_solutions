af732115717588f2fe82ac9b5022a45732df2e4e
Amol Mishra <amolsuraj.vit5@gmail.com>
2026-02-09 21:44:33 +0530

    Time: 35 ms (47.4%), Space: 24 MB (53.34%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -1,22 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
+    def inorder(self, node, vals):
+        if not node:
+            return
+        self.inorder(node.left, vals)
+        vals.append(node.val)
+        self.inorder(node.right, vals)
+
+    def build(self, vals, l, r):
+        if l > r:
+            return None
+        mid  = (l + r) // 2
+        node = TreeNode(vals[mid])
+        node.left  = self.build(vals, l, mid - 1)
+        node.right = self.build(vals, mid + 1, r)
+        return node
+
     def balanceBST(self, root):
-        
-        def inorder(node):
-            if not node: return []
-            return inorder(node.left) + [node.val] + inorder(node.right)
-        ns = inorder(root)
-        
-        def build(l, r):
-            if l > r: return None
-            m = (l + r) // 2
-            root = TreeNode(ns[m])
-            root.left, root.right = build(l, m-1), build(m + 1, r)
-            return root
-        
-        return build(0, len(ns) - 1)
+        vals = []
+        self.inorder(root, vals)
+        return self.build(vals, 0, len(vals) - 1)
