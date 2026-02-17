18355ab9e2a222d9e03dc7906579c484c59c16df
Amol Mishra <amolsuraj.vit5@gmail.com>
2026-02-17 20:18:11 +0530

    Time: 2 ms (32.01%), Space: 19.5 MB (39.62%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        res = []
+
+        for hour in range(12):
+            for minute in range(60):
+                if bin(hour).count("1") + bin(minute).count("1") == turnedOn: 
+                    res.append(f"{hour}:{minute:02d}")
+
+        return res
